da1dc20995e767027e6be78f8ec39984bec57d5d
Rueian <rueiancsie@gmail.com>
2021-10-12 23:10:07 +0800

    refactor: remove gomock

--- internal/test/cachemock/main.go
+++
@@ -1,3 +0,0 @@
-package cachemock
-
-//go:generate mockgen -destination=mock.go -package=cachemock github.com/rueian/rueidis/internal/cache Cache
--- internal/test/queuemock/main.go
+++
@@ -1,3 +0,0 @@
-package queuemock
-
-//go:generate mockgen -destination=mock.go -package=queuemock github.com/rueian/rueidis/internal/queue Queue
