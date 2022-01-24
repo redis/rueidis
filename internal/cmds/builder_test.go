9b40baa99d8a52ea3f23d5e9fb1fd0e48e254193
Rueian <rueiancsie@gmail.com>
2022-01-24 23:02:39 +0800

    test: improve flaky sync.Pool test case

--- internal/cmds/builder_test.go
+++ internal/cmds/builder_test.go
@@ -4,12 +4,13 @@ import "testing"
 
 func TestBuilder_Put(t *testing.T) {
 	builder := NewBuilder(InitSlot)
+retry:
 	cs1 := builder.get()
 	cs1.s = append(cs1.s, "1", "1", "1", "1", "1")
 	builder.Put(cs1)
 	cs2 := builder.get()
 	if cs1 != cs2 {
-		t.Fatalf("Put doesn't recycle the CommandSlice")
+		goto retry
 	}
 	if len(cs2.s) != 0 {
 		t.Fatalf("Put doesn't clean the CommandSlice")
