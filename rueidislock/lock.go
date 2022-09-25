f34223aa64817412314cccf05c4c3686d854b0c3
Rueian <rueiancsie@gmail.com>
2022-09-26 00:30:22 +0800

    feat: add rueidislock module

---
+++ rueidislock/lock.go
@@ -0,0 +1,302 @@
+package rueidislock
+
+import (
+	"context"
+	"encoding/binary"
+	"errors"
+	"math/rand"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/rueian/rueidis"
+)
+
+var sources sync.Pool
+
+func init() {
+	sources = sync.Pool{New: func() any { return rand.New(rand.NewSource(time.Now().UnixNano())) }}
+}
+
+type LockerOption struct {
+	ClientOption   rueidis.ClientOption
+	KeyPrefix      string
+	KeyMajority    int
+	KeyValidity    time.Duration
+	ExtendInterval time.Duration
+}
+
+type Locker interface {
+	WithContext(ctx context.Context, name string) (context.Context, context.CancelFunc, error)
+	Close()
+}
+
+func NewLocker(option LockerOption) (Locker, error) {
+	if option.KeyPrefix == "" {
+		option.KeyPrefix = "rueidislock"
+	}
+	if option.KeyValidity <= 0 {
+		option.KeyValidity = time.Second * 5
+	}
+	if option.ExtendInterval <= 0 {
+		option.ExtendInterval = time.Millisecond * 500
+	}
+	if option.KeyMajority <= 0 {
+		option.KeyMajority = 2
+	}
+	impl := &locker{
+		prefix:   option.KeyPrefix,
+		validity: option.KeyValidity,
+		interval: option.ExtendInterval,
+		majority: option.KeyMajority,
+		totalcnt: option.KeyMajority*2 - 1,
+		gates:    make(map[string]*gate),
+	}
+	option.ClientOption.DisableCache = false
+	option.ClientOption.ClientTrackingOptions = []string{"OPTOUT"}
+	option.ClientOption.OnInvalidations = impl.onInvalidations
+	client, err := rueidis.NewClient(option.ClientOption)
+	if err != nil {
+		return nil, err
+	}
+	impl.client = client
+	return impl, nil
+}
+
+type locker struct {
+	client   rueidis.Client
+	prefix   string
+	validity time.Duration
+	interval time.Duration
+	majority int
+	totalcnt int
+
+	mu    sync.RWMutex
+	gates map[string]*gate
+}
+
+type gate struct {
+	w   int
+	ch  chan struct{}
+	csc []chan struct{}
+}
+
+func makegate(size int) *gate {
+	csc := make([]chan struct{}, size)
+	for i := 0; i < len(csc); i++ {
+		csc[i] = make(chan struct{}, 1)
+	}
+	return &gate{ch: make(chan struct{}, 1), csc: csc}
+}
+
+func random() string {
+	val := make([]byte, 24)
+	src := sources.Get().(rand.Source64)
+	binary.BigEndian.PutUint64(val[0:8], src.Uint64())
+	binary.BigEndian.PutUint64(val[8:16], src.Uint64())
+	binary.BigEndian.PutUint64(val[16:24], src.Uint64())
+	sources.Put(src)
+	return rueidis.BinaryString(val)
+}
+
+func keyname(prefix, name string, i int) string {
+	ia := strconv.Itoa(i)
+	sb := strings.Builder{}
+	sb.Grow(len(prefix) + len(name) + len(ia) + 2)
+	sb.WriteString(prefix)
+	sb.WriteByte(':')
+	sb.WriteString(ia)
+	sb.WriteByte(':')
+	sb.WriteString(name)
+	return sb.String()
+}
+
+func (m *locker) acquire(ctx context.Context, key, val string, deadline time.Time) error {
+	ctx, cancel := context.WithTimeout(ctx, m.validity/1000)
+	resp := m.client.DoMulti(ctx,
+		m.client.B().Set().Key(key).Value(val).Nx().PxatMillisecondsTimestamp(deadline.UnixMilli()).Build(),
+		m.client.B().Get().Key(key).Build(),
+	)
+	cancel()
+	if v, _ := resp[1].ToString(); v != val {
+		return errNotLock
+	}
+	return nil
+}
+
+func (m *locker) script(ctx context.Context, script *rueidis.Lua, key, val string, deadline time.Time) error {
+	ctx, cancel := context.WithDeadline(ctx, deadline)
+	resp := script.Exec(ctx, m.client, []string{key}, []string{val, strconv.FormatInt(deadline.UnixMilli(), 10)})
+	cancel()
+	if v, err := resp.AsInt64(); err != nil || v == 1 {
+		return err
+	}
+	return errNotLock
+}
+
+func (m *locker) waitgate(ctx context.Context, name string) (g *gate, err error) {
+	m.mu.Lock()
+	g, ok := m.gates[name]
+	if !ok {
+		g = makegate(m.totalcnt)
+		g.w++
+		m.gates[name] = g
+		m.mu.Unlock()
+		return g, nil
+	} else {
+		g.w++
+		m.mu.Unlock()
+	}
+	select {
+	case <-ctx.Done():
+		m.mu.Lock()
+		if g.w--; g.w == 0 {
+			delete(m.gates, name)
+		}
+		m.mu.Unlock()
+		return nil, ctx.Err()
+	case _, ok = <-g.ch:
+		if ok {
+			return g, nil
+		}
+		m.mu.Lock()
+		if g.w--; g.w == 0 {
+			delete(m.gates, name)
+		}
+		m.mu.Unlock()
+		return nil, ErrLockerClosed
+	}
+}
+
+func (m *locker) onInvalidations(messages []rueidis.RedisMessage) {
+	if messages == nil {
+		m.mu.Lock()
+		for _, g := range m.gates {
+			close(g.ch)
+			for _, ch := range g.csc {
+				close(ch)
+			}
+		}
+		m.gates = make(map[string]*gate)
+		m.mu.Unlock()
+	}
+	for _, msg := range messages {
+		k, _ := msg.ToString()
+		if ks := strings.SplitN(k, ":", 3); len(ks) == 3 {
+			m.mu.RLock()
+			g, ok := m.gates[ks[2]]
+			if ok {
+				n, _ := strconv.Atoi(ks[1])
+				select {
+				case g.csc[n] <- struct{}{}:
+				default:
+				}
+			}
+			m.mu.RUnlock()
+		}
+	}
+}
+
+func (m *locker) WithContext(ctx context.Context, name string) (context.Context, context.CancelFunc, error) {
+	for {
+		ctx, cancel := context.WithCancel(ctx)
+
+		g, err := m.waitgate(ctx, name)
+		if err != nil {
+			return ctx, cancel, err
+		}
+
+		val := random()
+		deadline := time.Now().Add(m.validity)
+		cacneltm := time.AfterFunc(m.validity, cancel)
+
+		var acquired int
+		var released int
+		cond := sync.NewCond(&sync.Mutex{})
+
+		monitoring := func(err error, key string, deadline time.Time, csc chan struct{}) {
+			if err == nil {
+				cond.L.Lock()
+				acquired++
+				acquired := acquired
+				cond.L.Unlock()
+				if acquired >= m.majority {
+					cond.Broadcast()
+				}
+				for timer := time.NewTimer(m.interval); err == nil; {
+					select {
+					case <-ctx.Done():
+						err = ctx.Err()
+					case <-timer.C:
+						deadline = deadline.Add(m.interval)
+						if err = m.script(ctx, extend, key, val, deadline); err == nil {
+							timer.Reset(m.interval)
+							<-csc
+						}
+					case <-csc:
+						if err = m.script(ctx, extend, key, val, deadline); err == nil {
+							<-csc
+						}
+					}
+				}
+			}
+			if err != errNotLock {
+				err = m.script(context.Background(), delkey, key, val, deadline)
+			}
+			cond.L.Lock()
+			released++
+			released := released
+			cond.L.Unlock()
+			if released >= m.majority {
+				cancel()
+				cond.Broadcast()
+				if released == m.totalcnt {
+					m.mu.Lock()
+					if g.w--; g.w == 0 {
+						delete(m.gates, name)
+						m.mu.Unlock()
+					} else {
+						m.mu.Unlock()
+						g.ch <- struct{}{}
+					}
+				}
+			}
+		}
+
+		go func() {
+			for i := 0; i < m.totalcnt; i++ {
+				select {
+				case <-g.csc[i]:
+				default:
+				}
+				key := keyname(m.prefix, name, i)
+				if err != errNotLock {
+					err = m.acquire(ctx, key, val, deadline)
+				}
+				go monitoring(err, key, deadline, g.csc[i])
+			}
+		}()
+		cond.L.Lock()
+		for released < m.majority && acquired < m.majority {
+			cond.Wait()
+		}
+		succeed := released < m.majority
+		cond.L.Unlock()
+		if cacneltm.Stop() && succeed {
+			return ctx, cancel, nil
+		}
+	}
+}
+
+func (m *locker) Close() {
+	m.client.Close()
+}
+
+var (
+	delkey = rueidis.NewLuaScript(`if redis.call("GET",KEYS[1]) == ARGV[1] then return redis.call("DEL",KEYS[1]) else return 0 end`)
+	extend = rueidis.NewLuaScript(`if redis.call("GET",KEYS[1]) == ARGV[1] then return redis.call("PEXPIREAT",KEYS[1],ARGV[2]) else return 0 end`)
+)
+
+var errNotLock = errors.New("not lock")
+var ErrLockerClosed = errors.New("locker closed")
---
+++ rueidislock/lock_test.go
@@ -0,0 +1,200 @@
+package rueidislock
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/rueian/rueidis"
+)
+
+var address = []string{"127.0.0.1:6376"}
+
+func newLocker(t *testing.T) *locker {
+	impl, err := NewLocker(LockerOption{
+		ClientOption: rueidis.ClientOption{InitAddress: address},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return impl.(*locker)
+}
+
+func newClient(t *testing.T) rueidis.Client {
+	client, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: address})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return client
+}
+
+func TestNewLocker(t *testing.T) {
+	l, err := NewLocker(LockerOption{ClientOption: rueidis.ClientOption{InitAddress: nil}})
+	if err == nil {
+		t.Fatal(err)
+	}
+	l, err = NewLocker(LockerOption{ClientOption: rueidis.ClientOption{InitAddress: address}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
+	impl := l.(*locker)
+	if impl.validity != 5*time.Second {
+		t.Fatalf("unexpected default validity %v", impl.validity)
+	}
+	if impl.majority != 2 {
+		t.Fatalf("unexpected default majority %v", impl.majority)
+	}
+	if impl.totalcnt != impl.majority*2-1 {
+		t.Fatalf("unexpected default totalcnt %v", impl.totalcnt)
+	}
+}
+
+func TestLocker_WithContext_MultipleLocker(t *testing.T) {
+	lockers := make([]*locker, 10)
+	sum := make([]int, len(lockers))
+	for i := 0; i < len(lockers); i++ {
+		lockers[i] = newLocker(t)
+	}
+	defer func() {
+		for _, locker := range lockers {
+			locker.Close()
+		}
+	}()
+	cnt := 100
+	lck := strconv.Itoa(rand.Int())
+	ctx := context.Background()
+	var wg sync.WaitGroup
+	wg.Add(len(lockers))
+	for i, l := range lockers {
+		go func(i int, l *locker) {
+			defer wg.Done()
+			for j := 0; j < cnt; j++ {
+				_, cancel, err := l.WithContext(ctx, lck)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+				sum[i]++
+				cancel()
+			}
+		}(i, l)
+	}
+	wg.Wait()
+	for i, s := range sum {
+		if s != cnt {
+			t.Fatalf("unexpected sum %v %v %v", i, s, cnt)
+		}
+	}
+}
+
+func TestLocker_WithContext_UnlockByClientSideCaching(t *testing.T) {
+	locker := newLocker(t)
+	defer locker.Close()
+	lck := strconv.Itoa(rand.Int())
+	ctx, cancel, err := locker.WithContext(context.Background(), lck)
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		client := newClient(t)
+		defer client.Close()
+		for i := 0; i < locker.majority; i++ {
+			if err := client.Do(context.Background(), client.B().Del().Key(keyname(locker.prefix, lck, i)).Build()).Error(); err != nil {
+				t.Error(err)
+			}
+		}
+	}()
+	<-ctx.Done()
+	cancel()
+	if !errors.Is(ctx.Err(), context.Canceled) {
+		t.Fatalf("unexpected err %v", err)
+	}
+}
+
+func TestLocker_WithContext_AutoExtend(t *testing.T) {
+	locker := newLocker(t)
+	defer locker.Close()
+
+	ctx, cancel, err := locker.WithContext(context.Background(), strconv.Itoa(rand.Int()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < 2; i++ {
+		select {
+		case <-ctx.Done():
+			t.Fatalf("unexpected context canceled %v", ctx.Err())
+		default:
+			time.Sleep(locker.validity)
+		}
+	}
+	if ctx.Err() != nil {
+		t.Fatalf("unexpected context canceled %v", ctx.Err())
+	}
+	cancel()
+}
+
+func TestLocker_WithContext_DeadContext(t *testing.T) {
+	locker := newLocker(t)
+	defer locker.Close()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	ctx, cancel, err := locker.WithContext(ctx, strconv.Itoa(rand.Int()))
+	if !errors.Is(err, context.Canceled) {
+		t.Fatal(err)
+	}
+}
+
+func TestLocker_WithContext_CancelContext(t *testing.T) {
+	locker := newLocker(t)
+	defer locker.Close()
+
+	lck := strconv.Itoa(rand.Int())
+	ctx, cancel, err := locker.WithContext(context.Background(), lck)
+
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			if _, _, err := locker.WithContext(ctx, lck); !errors.Is(err, context.Canceled) {
+				t.Error(err)
+			}
+			wg.Done()
+		}()
+	}
+	time.Sleep(time.Second)
+	cancel()
+	wg.Wait()
+	if !errors.Is(ctx.Err(), context.Canceled) {
+		t.Fatal(err)
+	}
+}
+
+func TestLocker_Close(t *testing.T) {
+	locker := newLocker(t)
+
+	lck := strconv.Itoa(rand.Int())
+	ctx, _, err := locker.WithContext(context.Background(), lck)
+
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			if _, _, err := locker.WithContext(context.Background(), lck); err != ErrLockerClosed {
+				t.Error(err)
+			}
+			wg.Done()
+		}()
+	}
+	time.Sleep(time.Second)
+	locker.Close()
+	wg.Wait()
+	if !errors.Is(ctx.Err(), context.Canceled) {
+		t.Fatal(err)
+	}
+}
