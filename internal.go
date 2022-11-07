8ad38bcf7b2010e70dee33d0622bf90272b4d64e
Dmitry Sedykh <sedykh@gmail.com>
2022-11-08 02:03:36 +0300

    Exported synonyms for Completed, Cacheable ^ & Builder types

---
+++ internal.go
@@ -0,0 +1,10 @@
+package rueidis
+
+import "github.com/rueian/rueidis/internal/cmds"
+
+// Exported synonyms for command types.
+type (
+	Completed = cmds.Completed
+	Cacheable = cmds.Cacheable
+	Builder   = cmds.Builder
+)
