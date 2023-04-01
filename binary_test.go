d58e93dd68f277ca115b269c344abf6f6f24e738
Rueian <rueiancsie@gmail.com>
2023-04-01 21:43:59 +0800

    feat: VectorString helpers to build vector search queries

--- binary_test.go
+++ binary_test.go
@@ -2,6 +2,7 @@ package rueidis
 
 import (
 	"encoding/json"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -27,6 +28,32 @@ func TestJSONPanic(t *testing.T) {
 	a := &recursive{}
 	a.R = a
 	JSON(a)
+}
+
+func TestVectorString32(t *testing.T) {
+	for _, test := range [][]float32{
+		{},
+		{0, 0, 0, 0},
+		{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+		{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	} {
+		if !reflect.DeepEqual(test, ToVector32(VectorString32(test))) {
+			t.Fatalf("fail to convert %v", test)
+		}
+	}
+}
+
+func TestVectorString64(t *testing.T) {
+	for _, test := range [][]float64{
+		{},
+		{0, 0, 0, 0},
+		{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9},
+		{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+	} {
+		if !reflect.DeepEqual(test, ToVector64(VectorString64(test))) {
+			t.Fatalf("fail to convert %v", test)
+		}
+	}
 }
 
 type recursive struct {
