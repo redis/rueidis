0b124a57c1609318b5a9d9cd8b6ccd49a2aa7e27
Rueian <rueiancsie@gmail.com>
2023-04-15 17:12:07 +0800

    feat: remove RedisResult.AsBytes() to support go1.18

--- om/conv.go
+++ om/conv.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
-	"unsafe"
 
 	"github.com/redis/rueidis"
 )
@@ -165,7 +164,7 @@ var converters = struct {
 				return rueidis.BinaryString(value.Bytes()), true
 			},
 			StringToValue: func(value string) (reflect.Value, error) {
-				buf := unsafe.Slice(unsafe.StringData(value), len(value))
+				buf := []byte(value)
 				return reflect.ValueOf(buf), nil
 			},
 		},
