d9dae4503dd490cc2adf754662938f5d75eb69a3
Rueian <rueiancsie@gmail.com>
2023-05-06 21:46:37 +0800

    feat: downgrade otel to v1.14.0 to support go1.18

--- rueidisotel/trace.go
+++ rueidisotel/trace.go
@@ -9,6 +9,8 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/metric/instrument"
 	"go.opentelemetry.io/otel/trace"
 
 	"github.com/redis/rueidis"
@@ -31,7 +33,7 @@ func WithClient(client rueidis.Client, opts ...Option) rueidis.Client {
 		opt(o)
 	}
 	if o.meterProvider == nil {
-		o.meterProvider = otel.GetMeterProvider() // Default to global MeterProvider
+		o.meterProvider = global.MeterProvider() // Default to global MeterProvider
 	}
 	if o.tracerProvider == nil {
 		o.tracerProvider = otel.GetTracerProvider() // Default to global TracerProvider
@@ -84,8 +86,8 @@ type otelclient struct {
 	tracerProvider trace.TracerProvider
 	tracer         trace.Tracer
 	meter          metric.Meter
-	cscMiss        metric.Int64Counter
-	cscHits        metric.Int64Counter
+	cscMiss        instrument.Int64Counter
+	cscHits        instrument.Int64Counter
 }
 
 func (o *otelclient) B() rueidis.Builder {
@@ -111,9 +113,9 @@ func (o *otelclient) DoCache(ctx context.Context, cmd rueidis.Cacheable, ttl tim
 	resp = o.client.DoCache(ctx, cmd, ttl)
 	if resp.NonRedisError() == nil {
 		if resp.IsCacheHit() {
-			o.cscHits.Add(ctx, 1, metric.WithAttributes(o.tAttrs...))
+			o.cscHits.Add(ctx, 1, o.tAttrs...)
 		} else {
-			o.cscMiss.Add(ctx, 1, metric.WithAttributes(o.tAttrs...))
+			o.cscMiss.Add(ctx, 1, o.tAttrs...)
 		}
 	}
 	o.end(span, resp.Error())
@@ -126,9 +128,9 @@ func (o *otelclient) DoMultiCache(ctx context.Context, multi ...rueidis.Cacheabl
 	for _, resp := range resps {
 		if resp.NonRedisError() == nil {
 			if resp.IsCacheHit() {
-				o.cscHits.Add(ctx, 1, metric.WithAttributes(o.tAttrs...))
+				o.cscHits.Add(ctx, 1, o.tAttrs...)
 			} else {
-				o.cscMiss.Add(ctx, 1, metric.WithAttributes(o.tAttrs...))
+				o.cscMiss.Add(ctx, 1, o.tAttrs...)
 			}
 		}
 	}
@@ -200,8 +202,8 @@ type dedicated struct {
 	tAttrs  []attribute.KeyValue
 	tracer  trace.Tracer
 	meter   metric.Meter
-	cscMiss metric.Int64Counter
-	cscHits metric.Int64Counter
+	cscMiss instrument.Int64Counter
+	cscHits instrument.Int64Counter
 }
 
 func (d *dedicated) B() rueidis.Builder {
