83a730f546ade77dcb6d40ca72aba6eff329875c
Rueian <rueiancsie@gmail.com>
2023-05-13 23:44:31 +0800

    test: retry github action test

--- dockertest.sh
+++ dockertest.sh
@@ -5,7 +5,7 @@ set -ev
 package=${1:-./...}
 pkgbase="github.com/redis/rueidis"
 
+trap "docker-compose down -v" EXIT
 [[ "/om /rueidiscompat /rueidislock /rueidisotel ./..." =~ "${package#$pkgbase}" || -z "${package#$pkgbase}" ]] && \
 docker-compose up -d
 go test -coverprofile=coverage.out -v -race -timeout 30m $package
-docker-compose down -v
