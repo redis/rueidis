fefc37d14e8bc1117492a4a87c6572c123486a38
Mohammad Fatemi <mr.smf8@gmail.com>
2023-05-19 11:56:00 +0330

    feat: Add ReadOnlyReplica client option

--- rueidis.go
+++ rueidis.go
@@ -135,6 +135,10 @@ type ClientOption struct {
 	AlwaysPipelining bool
 	// AlwaysRESP2 makes rueidis.Client always uses RESP2, otherwise it will try using RESP3 first.
 	AlwaysRESP2 bool
+
+	// ReadOnlyReplica indicates that this client will only try to connect to readonly replicas of redis setup.
+	// currently, it is only implemented for sentinel client
+	ReadOnlyReplica bool
 }
 
 // SentinelOption contains MasterSet,
