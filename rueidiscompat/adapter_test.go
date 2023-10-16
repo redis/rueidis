161c85ca34559ccdd65d4b654b77e01ddc79b9d4
unknowntpo <e850506@gmail.com>
2023-10-16 10:33:41 +0800

    scaffolding tests

--- rueidiscompat/adapter_test.go
+++ rueidiscompat/adapter_test.go
@@ -8741,10 +8741,100 @@ func testAdapterCache(resp3 bool) {
 	})
 
 	Describe("GearsCmdable", func() {
-		It("should build commands", Pending, func() {
+		BeforeEach(func() {
+			Expect(adapter.FlushDB(ctx).Err()).NotTo(HaveOccurred())
+			adapter.TFunctionDelete(ctx, "lib1")
+		})
+		// Copied from go-redis
+		// https://github.com/redis/go-redis/blob/f994ff1cd96299a5c8029ae3403af7b17ef06e8a/gears_commands_test.go
+		It("should TFunctionLoad, TFunctionLoadArgs and TFunctionDelete ", Label("gears", "tfunctionload"), func() {
+			resultAdd, err := adapter.TFunctionLoad(ctx, libCode("lib1")).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("OK"))
+			opt := &TFunctionLoadOptions{Replace: true, Config: `{"last_update_field_name":"last_update"}`}
+			resultAdd, err = adapter.TFunctionLoadArgs(ctx, libCodeWithConfig("lib1"), opt).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("OK"))
+		})
+		It("should TFunctionList", Label("gears", "tfunctionlist"), func() {
+			resultAdd, err := adapter.TFunctionLoad(ctx, libCode("lib1")).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("OK"))
+			resultList, err := adapter.TFunctionList(ctx).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultList[0]["engine"]).To(BeEquivalentTo("js"))
+			opt := &TFunctionListOptions{Withcode: true, Verbose: 2}
+			resultListArgs, err := adapter.TFunctionListArgs(ctx, opt).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultListArgs[0]["code"]).NotTo(BeEquivalentTo(""))
+		})
 
+		It("should TFCall", Label("gears", "tfcall"), func() {
+			var resultAdd interface{}
+			resultAdd, err := adapter.TFunctionLoad(ctx, libCode("lib1")).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("OK"))
+			resultAdd, err = adapter.TFCall(ctx, "lib1", "foo", 0).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("bar"))
+		})
+
+		It("should TFCallArgs", Label("gears", "tfcallargs"), func() {
+			var resultAdd interface{}
+			resultAdd, err := adapter.TFunctionLoad(ctx, libCode("lib1")).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("OK"))
+			opt := &TFCallOptions{Arguments: []string{"foo", "bar"}}
+			resultAdd, err = adapter.TFCallArgs(ctx, "lib1", "foo", 0, opt).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("bar"))
+		})
+
+		It("should TFCallASYNC", Label("gears", "TFCallASYNC"), func() {
+			var resultAdd interface{}
+			resultAdd, err := adapter.TFunctionLoad(ctx, libCode("lib1")).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("OK"))
+			resultAdd, err = adapter.TFCallASYNC(ctx, "lib1", "foo", 0).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("bar"))
+		})
+
+		It("should TFCallASYNCArgs", Label("gears", "TFCallASYNCargs"), func() {
+			var resultAdd interface{}
+			resultAdd, err := adapter.TFunctionLoad(ctx, libCode("lib1")).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("OK"))
+			opt := &TFCallOptions{Arguments: []string{"foo", "bar"}}
+			resultAdd, err = adapter.TFCallASYNCArgs(ctx, "lib1", "foo", 0, opt).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resultAdd).To(BeEquivalentTo("bar"))
 		})
 	})
+}
+
+func libCode(libName string) string {
+	return fmt.Sprintf("#!js api_version=1.0 name=%s\n redis.registerFunction('foo', ()=>{{return 'bar'}})", libName)
+}
+
+func libCodeWithConfig(libName string) string {
+	lib := `#!js api_version=1.0 name=%s
+
+	var last_update_field_name = "__last_update__"
+	
+	if (redis.config.last_update_field_name !== undefined) {
+		if (typeof redis.config.last_update_field_name != 'string') {
+			throw "last_update_field_name must be a string";
+		}
+		last_update_field_name = redis.config.last_update_field_name
+	}
+	
+	redis.registerFunction("hset", function(client, key, field, val){
+		// get the current time in ms
+		var curr_time = client.call("time")[0];
+		return client.call('hset', key, field, val, last_update_field_name, curr_time);
+	});`
+	return fmt.Sprintf(lib, libName)
 }
 
 type numberStruct struct {
