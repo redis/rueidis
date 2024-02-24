bf57e8007059ca10c984889df00271d6d0098f6b
Rueian <rueiancsie@gmail.com>
2024-02-24 10:58:57 +0800

    feat: bump v1.0.29

--- mock/go.mod
+++ mock/go.mod
@@ -5,7 +5,7 @@ go 1.20
 replace github.com/redis/rueidis => ../
 
 require (
-	github.com/redis/rueidis v1.0.28
+	github.com/redis/rueidis v1.0.29
 	go.uber.org/mock v0.3.0
 )
 
--- pipe.go
+++ pipe.go
@@ -21,7 +21,7 @@ import (
 )
 
 const LibName = "rueidis"
-const LibVer = "1.0.28"
+const LibVer = "1.0.29"
 
 var noHello = regexp.MustCompile("unknown command .?(HELLO|hello).?")
 
