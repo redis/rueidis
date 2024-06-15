6787b03cc546c8cf8ffa25431291ac615df3ce99
neelpatel04466 <163070713+neelpatel04466@users.noreply.github.com>
2024-06-15 12:30:34 +0500

    Update message.go

--- message.go
+++ message.go
@@ -725,23 +725,28 @@ func (m *RedisMessage) AsStrSlice() ([]string, error) {
 
 // AsIntSlice check if message is a redis array/set response, and convert to []int64.
 // redis nil element and other non integer element will be present as zero.
-func (m *RedisMessage) AsIntSlice() ([]int64, error) {
-	values, err := m.ToArray()
-	if err != nil {
-		return nil, err
-	}
-	s := make([]int64, len(values))
-	for i, v := range values {
-		if len(v.string) != 0 {
-			if s[i], err = strconv.ParseInt(v.string, 10, 64); err != nil {
-				return nil, err
-			}
-		} else {
-			s[i] = v.integer
-		}
-	}
-	return s, nil
+func (r RedisResult) AsIntSlice() ([]int64, error) {
+    if r.err != nil {
+        return nil, r.err
+    }
+
+    values, err := r.val.ToArray()
+    if err != nil {
+        return nil, err
+    }
+    s := make([]int64, len(values))
+    for i, v := range values {
+        if len(v.string) != 0 {
+            if s[i], err = strconv.ParseInt(v.string, 10, 64); err != nil {
+                return nil, err
+            }
+        } else {
+            s[i] = v.integer
+        }
+    }
+    return s, nil
 }
+
 
 // AsFloatSlice check if message is a redis array/set response, and convert to []float64.
 // redis nil element and other non float element will be present as zero.
