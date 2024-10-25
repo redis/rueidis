0b00084f56a2b3ecf4602fa8a88f6abc642af40f
vatsal <vatsal.v.anand@gmail.com>
2024-10-24 23:34:38 -0700

    Add FT.ALTER in OM JSON and Hash repo Signed-off-by: Vatsal <vatsal.v.anand@gmail.com>

--- om/json_test.go
+++ om/json_test.go
@@ -2,6 +2,7 @@ package om
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -202,6 +203,85 @@ func TestNewJSONRepository(t *testing.T) {
 			_, err = repo.FetchCache(ctx, e.Key, time.Minute)
 			if !IsRecordNotFound(err) {
 				t.Fatalf("should not be found, but got %v", e)
+			}
+		})
+
+		t.Run("AlterIndex", func(t *testing.T) {
+			err := repo.CreateIndex(ctx, func(schema FtCreateSchema) rueidis.Completed {
+				return schema.FieldName("$.Val").Text().Build()
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			time.Sleep(time.Second)
+
+			var entities []*JSONTestStruct
+			for i := 3; i >= 1; i-- {
+				e := repo.NewEntity()
+				e.Val = []byte("some")
+				e.Nested = struct {
+					F1 string
+				}{
+					F1: fmt.Sprintf("%d", i),
+				}
+
+				err = repo.Save(ctx, e)
+				if err != nil {
+					t.Fatal(err)
+				}
+				entities = append(entities, e)
+			}
+
+			time.Sleep(time.Second)
+
+			n, records, err := repo.Search(ctx, func(search FtSearchIndex) rueidis.Completed {
+				return search.Query("*").Build()
+			})
+
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != 3 {
+				t.Fatalf("unexpected total count %v", n)
+			}
+			if len(records) != 3 {
+				t.Fatalf("unexpected return count %v", n)
+			}
+			if !reflect.DeepEqual(entities[0], records[0]) {
+				t.Fatalf("entities[0] should be the same as e[0]")
+			}
+
+			err = repo.AlterIndex(ctx, func(alter FtAlterIndex) rueidis.Completed {
+				return alter.
+					Schema().Add().Field("$.Nested.F1").Options("TEXT").
+					Build()
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			n, records, err = repo.Search(ctx, func(search FtSearchIndex) rueidis.Completed {
+				return search.Query("*").Sortby("$.Nested.F1").Build()
+			})
+
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != 3 {
+				t.Fatalf("unexpected total count %v", n)
+			}
+			if len(records) != 3 {
+				t.Fatalf("unexpected return count %v", n)
+			}
+			if !reflect.DeepEqual(entities[2], records[0]) {
+				t.Fatalf("entities[0] should be the same as e[2]")
+			}
+
+			time.Sleep(time.Second)
+
+			if err = repo.DropIndex(ctx); err != nil {
+				t.Fatal(err)
 			}
 		})
 	})
