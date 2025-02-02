fe5c09ecb9de9b87059704847c58710949bc8589
Rueian <rueiancsie@gmail.com>
2025-02-02 11:47:51 -0800

    feat: bump v1.0.54
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- mock/go.mod
+++ mock/go.mod
@@ -5,7 +5,7 @@ go 1.22.0
 replace github.com/redis/rueidis => ../
 
 require (
-	github.com/redis/rueidis v1.0.53
+	github.com/redis/rueidis v1.0.54
 	go.uber.org/mock v0.5.0
 )
 
--- pipe.go
+++ pipe.go
@@ -21,7 +21,7 @@ import (
 )
 
 const LibName = "rueidis"
-const LibVer = "1.0.53"
+const LibVer = "1.0.54"
 
 var noHello = regexp.MustCompile("unknown command .?(HELLO|hello).?")
 
