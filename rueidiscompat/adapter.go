f065b191d3ee68a622b3e86e666e909e91344f7e
astorig <a9086666032@gmail.com>
2025-02-10 19:35:26 -0300

    feat(issue-742): add ACLCat to rueidiscompat
    Signed-off-by: astorig <a9086666032@gmail.com>

--- rueidiscompat/adapter.go
+++ rueidiscompat/adapter.go
@@ -419,7 +419,6 @@ type CoreCmdable interface {
 	ACLSetUser(ctx context.Context, username string, rules ...string) *StatusCmd
 	ACLLogReset(ctx context.Context) *StatusCmd
 
-
 	ModuleLoadex(ctx context.Context, conf *ModuleLoadexConfig) *StringCmd
 	GearsCmdable
 	ProbabilisticCmdable
@@ -3215,10 +3214,17 @@ func (c *Compat) ACLSetUser(ctx context.Context, username string, rules ...strin
 	resp := c.client.Do(ctx, cmd)
 	return newStatusCmd(resp)
 }
+
 func (c *Compat) ACLLogReset(ctx context.Context) *StatusCmd {
 	cmd := c.client.B().AclLog().Reset().Build()
 	resp := c.client.Do(ctx, cmd)
 	return newStatusCmd(resp)
+}
+
+func (c *Compat) ACLCat(ctx context.Context) *StringSliceCmd {
+	cmd := c.client.B().AclCat().Build()
+	resp := c.client.Do(ctx, cmd)
+	return newStringSliceCmd(resp)
 }
 
 func (c *Compat) doPrimaries(ctx context.Context, fn func(c rueidis.Client) error) error {
