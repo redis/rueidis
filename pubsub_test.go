f2f7b750a5cbdb60a9c73b00d74e02faa77e3f1d
Rueian <rueiancsie@gmail.com>
2025-06-02 17:55:09 -0700

    feat: add the new WithOnSubscriptionHook for the Receive method (#846)
    * feat: add the new WithOnSubscriptionHook for the Receive method
    
    Signed-off-by: Rueian <rueiancsie@gmail.com>
    
    * feat: apply betteraligment
    
    Signed-off-by: Rueian <rueiancsie@gmail.com>
    
    * feat: add the new WithOnSubscriptionHook for the Receive method
    
    Signed-off-by: Rueian <rueiancsie@gmail.com>
    
    * feat: add the new WithOnSubscriptionHook for the Receive method
    
    Signed-off-by: Rueian <rueiancsie@gmail.com>
    
    ---------
    
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- pubsub_test.go
+++ pubsub_test.go
@@ -14,9 +14,24 @@ func TestSubs_Publish(t *testing.T) {
 
 	t.Run("with multiple subs", func(t *testing.T) {
 		s := newSubs()
-		ch1, cancel1 := s.Subscribe([]string{"a"})
-		ch2, cancel2 := s.Subscribe([]string{"a"})
-		ch3, cancel3 := s.Subscribe([]string{"b"})
+		counts := map[string]int{
+			"a": 0,
+			"b": 0,
+		}
+		subFn := func(s PubSubSubscription) {
+			counts[s.Channel]++
+		}
+
+		ch1, cancel1 := s.Subscribe([]string{"a"}, subFn)
+		ch2, cancel2 := s.Subscribe([]string{"a"}, subFn)
+		ch3, cancel3 := s.Subscribe([]string{"b"}, subFn)
+		s.Confirm(PubSubSubscription{Channel: "a"})
+		s.Confirm(PubSubSubscription{Channel: "b"})
+
+		if counts["a"] != 2 || counts["b"] != 1 {
+			t.Fatalf("unexpected counts %v", counts)
+		}
+
 		m1 := PubSubMessage{Pattern: "1", Channel: "2", Message: "3"}
 		m2 := PubSubMessage{Pattern: "11", Channel: "22", Message: "33"}
 		go func() {
@@ -45,7 +60,7 @@ func TestSubs_Publish(t *testing.T) {
 
 	t.Run("drain ch", func(t *testing.T) {
 		s := newSubs()
-		ch, cancel := s.Subscribe([]string{"a"})
+		ch, cancel := s.Subscribe([]string{"a"}, nil)
 		s.Publish("a", PubSubMessage{})
 		if len(ch) != 1 {
 			t.Fatalf("unexpected ch len %v", len(ch))
@@ -60,7 +75,11 @@ func TestSubs_Publish(t *testing.T) {
 func TestSubs_Unsubscribe(t *testing.T) {
 	defer ShouldNotLeaked(SetupLeakDetection())
 	s := newSubs()
-	ch, _ := s.Subscribe([]string{"1", "2"})
+	counts := map[string]int{"1": 0, "2": 0}
+	subFn := func(s PubSubSubscription) {
+		counts[s.Channel]++
+	}
+	ch, _ := s.Subscribe([]string{"1", "2"}, subFn)
 	go func() {
 		s.Publish("1", PubSubMessage{})
 	}()
@@ -68,7 +87,10 @@ func TestSubs_Unsubscribe(t *testing.T) {
 	if !ok {
 		t.Fatalf("unexpected ch closed")
 	}
-	s.Unsubscribe("1")
+	s.Unsubscribe(PubSubSubscription{Channel: "1"})
+	if counts["1"] != 1 {
+		t.Fatalf("unexpected counts %v", counts)
+	}
 	_, ok = <-ch
 	if ok {
 		t.Fatalf("unexpected ch unclosed")
