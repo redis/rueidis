valkey-io/valkey-go

Ensure deterministic routing in _pickMulti and _pickMultiCache (#52)
* Ensure deterministic routing in _pickMulti and _pickMultiCache

The _pickMulti and _pickMultiCache functions previously invoked
the SendToReplicas callback twice for the same batch of commands:
once to count commands per connection and a second time to assign
them.

If the SendToReplicas implementation was non-deterministic
(e.g., stateful, like a round-robin counter), this could lead to
inconsistent routing decisions between the two loops. This could
potentially cause a panic if a nil connection was chosen on the
second pass.

This change refactors both functions to call SendToReplicas only
once per command. The chosen destination connection for each
command is stored in a temporary slice, which is then used for
dispatching. This guarantees that the routing decision is consistent
for the entire DoMulti or DoMultiCache operation.

A test case with a stateful SendToReplicas function has been added
to verify the fix and prevent regressions in both code paths.

Signed-off-by: Ping Xie <pingxie@google.com>

* reduce heap allocations

Signed-off-by: Ping Xie <pingxie@google.com>

* test: SendToReplicas with large DoMulti

Signed-off-by: Rueian <rueiancsie@gmail.com>

---------

Signed-off-by: Ping Xie <pingxie@google.com>
Signed-off-by: Rueian <rueiancsie@gmail.com>
Co-authored-by: Rueian <rueiancsie@gmail.com>

cluster_test.go
@@ -8045,3 +8045,143 @@ func TestClusterClientConnLifetime(t *testing.T) {
 		}
 	})
 }
+
+//gocyclo:ignore
+func TestClusterClient_SendToAlternatePrimaryAndReplicaNodes(t *testing.T) {
+	defer ShouldNotLeaked(SetupLeakDetection())
+
+	primaryNodeConn := &mockConn{
+		DoMultiFn: func(multi ...Completed) *valkeyresults {
+			resps := make([]ValkeyResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Commands(), " ")), nil)
+			}
+			return &valkeyresults{s: resps}
+		},
+		DoMultiCacheFn: func(multi ...CacheableTTL) *valkeyresults {
+			resps := make([]ValkeyResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Cmd.Commands(), " ")), nil)
+			}
+			return &valkeyresults{s: resps}
+		},
+		DoOverride: map[string]func(cmd Completed) ValkeyResult{
+			"CLUSTER SLOTS": func(cmd Completed) ValkeyResult {
+				return slotsMultiResp
+			},
+			"INFO": func(cmd Completed) ValkeyResult {
+				return newResult(strmsg('+', "INFO"), nil)
+			},
+			"GET K1{a}": func(cmd Completed) ValkeyResult {
+				return newResult(strmsg('+', "GET K1{a}"), nil)
+			},
+			"GET K2{b}": func(cmd Completed) ValkeyResult {
+				return newResult(strmsg('+', "GET K2{b}"), nil)
+			},
+		},
+	}
+	replicaNodeConn := &mockConn{
+		DoMultiFn: func(multi ...Completed) *valkeyresults {
+			resps := make([]ValkeyResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Commands(), " ")), nil)
+			}
+			return &valkeyresults{s: resps}
+		},
+		DoMultiCacheFn: func(multi ...CacheableTTL) *valkeyresults {
+			resps := make([]ValkeyResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Cmd.Commands(), " ")), nil)
+			}
+			return &valkeyresults{s: resps}
+		},
+		DoOverride: map[string]func(cmd Completed) ValkeyResult{
+			"GET Do": func(cmd Completed) ValkeyResult {
+				return newResult(strmsg('+', "GET Do"), nil)
+			},
+			"GET K1{a}": func(cmd Completed) ValkeyResult {
+				return newResult(strmsg('+', "GET K1{a}"), nil)
+			},
+			"GET K2{b}": func(cmd Completed) ValkeyResult {
+				return newResult(strmsg('+', "GET K2{b}"), nil)
+			},
+		},
+		DoCacheOverride: map[string]func(cmd Cacheable, ttl time.Duration) ValkeyResult{
+			"GET DoCache": func(cmd Cacheable, ttl time.Duration) ValkeyResult {
+				return newResult(strmsg('+', "GET DoCache"), nil)
+			},
+		},
+	}
+
+	nextNode := -1
+	client, err := newClusterClient(
+		&ClientOption{
+			InitAddress: []string{"127.0.0.1:0"},
+			SendToReplicas: func(cmd Completed) bool {
+				nextNode++
+				return (nextNode/2) % 2 == 0
+			},
+		},
+		func(dst string, opt *ClientOption) conn {
+			if dst == "127.0.0.1:0" || dst == "127.0.2.1:0" { // primary nodes
+				return primaryNodeConn
+			} else { // replica nodes
+				return replicaNodeConn
+			}
+		},
+		newRetryer(defaultRetryDelayFn),
+	)
+	if err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	t.Run("DoMulti Multi Slot", func(t *testing.T) {
+		c1 := client.B().Get().Key("K1{a}").Build()
+		c2 := client.B().Get().Key("K2{b}").Build()
+		resps := client.DoMulti(context.Background(), c1, c2)
+		if v, err := resps[0].ToString(); err != nil || v != "GET K1{a}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[1].ToString(); err != nil || v != "GET K2{b}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+	})
+
+	t.Run("DoMulti Multi Slot Large", func(t *testing.T) {
+		var cmds []Completed
+		for i := 0; i < 500; i++ {
+			cmds = append(cmds, client.B().Get().Key("K1{a}").Build())
+		}
+		resps := client.DoMulti(context.Background(), cmds...)
+		for _, resp := range resps {
+			if v, err := resp.ToString(); err != nil || v != "GET K1{a}" {
+				t.Fatalf("unexpected response %v %v", v, err)
+			}
+		}
+	})
+
+	t.Run("DoMultiCache Multi Slot", func(t *testing.T) {
+		c1 := client.B().Get().Key("K1{a}").Cache()
+		c2 := client.B().Get().Key("K2{b}").Cache()
+		resps := client.DoMultiCache(context.Background(), CT(c1, time.Second), CT(c2, time.Second))
+		if v, err := resps[0].ToString(); err != nil || v != "GET K1{a}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[1].ToString(); err != nil || v != "GET K2{b}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+	})
+
+	t.Run("DoMultiCache Multi Slot Large", func(t *testing.T) {
+		var cmds []CacheableTTL
+		for i := 0; i < 500; i++ {
+			cmds = append(cmds, CT(client.B().Get().Key("K1{a}").Cache(), time.Second))
+		}
+		resps := client.DoMultiCache(context.Background(), cmds...)
+		for _, resp := range resps {
+			if v, err := resp.ToString(); err != nil || v != "GET K1{a}" {
+				t.Fatalf("unexpected response %v %v", v, err)
+			}
+		}
+	})
+}

redis/rueidis

cluster_test.go
@@ -8045,3 +8045,143 @@ func TestClusterClientConnLifetime(t *testing.T) {
 		}
 	})
 }
+
+//gocyclo:ignore
+func TestClusterClient_SendToAlternatePrimaryAndReplicaNodes(t *testing.T) {
+	defer ShouldNotLeaked(SetupLeakDetection())
+
+	primaryNodeConn := &mockConn{
+		DoMultiFn: func(multi ...Completed) *redisresults {
+			resps := make([]RedisResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Commands(), " ")), nil)
+			}
+			return &redisresults{s: resps}
+		},
+		DoMultiCacheFn: func(multi ...CacheableTTL) *redisresults {
+			resps := make([]RedisResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Cmd.Commands(), " ")), nil)
+			}
+			return &redisresults{s: resps}
+		},
+		DoOverride: map[string]func(cmd Completed) RedisResult{
+			"CLUSTER SLOTS": func(cmd Completed) RedisResult {
+				return slotsMultiResp
+			},
+			"INFO": func(cmd Completed) RedisResult {
+				return newResult(strmsg('+', "INFO"), nil)
+			},
+			"GET K1{a}": func(cmd Completed) RedisResult {
+				return newResult(strmsg('+', "GET K1{a}"), nil)
+			},
+			"GET K2{b}": func(cmd Completed) RedisResult {
+				return newResult(strmsg('+', "GET K2{b}"), nil)
+			},
+		},
+	}
+	replicaNodeConn := &mockConn{
+		DoMultiFn: func(multi ...Completed) *redisresults {
+			resps := make([]RedisResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Commands(), " ")), nil)
+			}
+			return &redisresults{s: resps}
+		},
+		DoMultiCacheFn: func(multi ...CacheableTTL) *redisresults {
+			resps := make([]RedisResult, len(multi))
+			for i, cmd := range multi {
+				resps[i] = newResult(strmsg('+', strings.Join(cmd.Cmd.Commands(), " ")), nil)
+			}
+			return &redisresults{s: resps}
+		},
+		DoOverride: map[string]func(cmd Completed) RedisResult{
+			"GET Do": func(cmd Completed) RedisResult {
+				return newResult(strmsg('+', "GET Do"), nil)
+			},
+			"GET K1{a}": func(cmd Completed) RedisResult {
+				return newResult(strmsg('+', "GET K1{a}"), nil)
+			},
+			"GET K2{b}": func(cmd Completed) RedisResult {
+				return newResult(strmsg('+', "GET K2{b}"), nil)
+			},
+		},
+		DoCacheOverride: map[string]func(cmd Cacheable, ttl time.Duration) RedisResult{
+			"GET DoCache": func(cmd Cacheable, ttl time.Duration) RedisResult {
+				return newResult(strmsg('+', "GET DoCache"), nil)
+			},
+		},
+	}
+
+	nextNode := -1
+	client, err := newClusterClient(
+		&ClientOption{
+			InitAddress: []string{"127.0.0.1:0"},
+			SendToReplicas: func(cmd Completed) bool {
+				nextNode++
+				return (nextNode/2)%2 == 0
+			},
+		},
+		func(dst string, opt *ClientOption) conn {
+			if dst == "127.0.0.1:0" || dst == "127.0.2.1:0" { // primary nodes
+				return primaryNodeConn
+			} else { // replica nodes
+				return replicaNodeConn
+			}
+		},
+		newRetryer(defaultRetryDelayFn),
+	)
+	if err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	t.Run("DoMulti Multi Slot", func(t *testing.T) {
+		c1 := client.B().Get().Key("K1{a}").Build()
+		c2 := client.B().Get().Key("K2{b}").Build()
+		resps := client.DoMulti(context.Background(), c1, c2)
+		if v, err := resps[0].ToString(); err != nil || v != "GET K1{a}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[1].ToString(); err != nil || v != "GET K2{b}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+	})
+
+	t.Run("DoMulti Multi Slot Large", func(t *testing.T) {
+		var cmds []Completed
+		for i := 0; i < 500; i++ {
+			cmds = append(cmds, client.B().Get().Key("K1{a}").Build())
+		}
+		resps := client.DoMulti(context.Background(), cmds...)
+		for _, resp := range resps {
+			if v, err := resp.ToString(); err != nil || v != "GET K1{a}" {
+				t.Fatalf("unexpected response %v %v", v, err)
+			}
+		}
+	})
+
+	t.Run("DoMultiCache Multi Slot", func(t *testing.T) {
+		c1 := client.B().Get().Key("K1{a}").Cache()
+		c2 := client.B().Get().Key("K2{b}").Cache()
+		resps := client.DoMultiCache(context.Background(), CT(c1, time.Second), CT(c2, time.Second))
+		if v, err := resps[0].ToString(); err != nil || v != "GET K1{a}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+		if v, err := resps[1].ToString(); err != nil || v != "GET K2{b}" {
+			t.Fatalf("unexpected response %v %v", v, err)
+		}
+	})
+
+	t.Run("DoMultiCache Multi Slot Large", func(t *testing.T) {
+		var cmds []CacheableTTL
+		for i := 0; i < 500; i++ {
+			cmds = append(cmds, CT(client.B().Get().Key("K1{a}").Cache(), time.Second))
+		}
+		resps := client.DoMultiCache(context.Background(), cmds...)
+		for _, resp := range resps {
+			if v, err := resp.ToString(); err != nil || v != "GET K1{a}" {
+				t.Fatalf("unexpected response %v %v", v, err)
+			}
+		}
+	})
+}