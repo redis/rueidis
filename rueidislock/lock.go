30fced164060fb83d25eb16d7c8af71f88f067ed
David Linehan <169060848+davidlin-tv2@users.noreply.github.com>
2025-07-04 04:38:57 +0200

    docs: update valkeylock comment (#57)
    Updates comment on default value of ExtendInterval in valkeylock to
    match the actual default value used.
    
    Signed-off-by: David Linehan <169060848+davidlin-tv2@users.noreply.github.com>
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- rueidislock/lock.go
+++ rueidislock/lock.go
@@ -24,7 +24,7 @@ type LockerOption struct {
 	ClientOption rueidis.ClientOption
 	// KeyValidity is the validity duration of locks and will be extended periodically by the ExtendInterval. The default value is 5s.
 	KeyValidity time.Duration
-	// ExtendInterval is the interval to extend KeyValidity. Default value is 1s.
+	// ExtendInterval is the interval to extend KeyValidity. Default value is 1/2 of KeyValidity.
 	ExtendInterval time.Duration
 	// TryNextAfter is the timeout duration before trying the next redis key for locks. The default value is 20ms.
 	TryNextAfter time.Duration
