valkey-io/valkey-go

feat: client capa redirect (#82)
* feat: client capa redirect impl

Signed-off-by: SoulPancake <angbpy@gmail.com>

* feat: pin early
Signed-off-by: Anurag Bandyopadhyay <angbpy@gmail.com>

Signed-off-by: SoulPancake <angbpy@gmail.com>

* Add tests for pipe.go and standalone.go redirect coverage

Fix DoMultiCache test to avoid build-twice error

Signed-off-by: SoulPancake <angbpy@gmail.com>

---------

Signed-off-by: SoulPancake <angbpy@gmail.com>

message.go
@@ -88,6 +88,14 @@ func (r *ValkeyError) IsAsk() (addr string, ok bool) {
 	return
 }
 
+// IsRedirect checks if it is a valkey REDIRECT message and returns redirect address.
+func (r *ValkeyError) IsRedirect() (addr string, ok bool) {
+	if ok = strings.HasPrefix(r.string(), "REDIRECT"); ok {
+		addr = fixIPv6HostPort(strings.Split(r.string(), " ")[1])
+	}
+	return
+}
+
 func fixIPv6HostPort(addr string) string {
 	if strings.IndexByte(addr, '.') < 0 && len(addr) > 0 && addr[0] != '[' { // skip ipv4 and enclosed ipv6
 		if i := strings.LastIndexByte(addr, ':'); i >= 0 {

message_test.go
@@ -109,6 +109,41 @@ func TestValkeyErrorIsAsk(t *testing.T) {
 	}
 }
 
+func TestValkeyErrorIsRedirect(t *testing.T) {
+	for _, c := range []struct {
+		err  string
+		addr string
+	}{
+		{err: "REDIRECT 127.0.0.1:6380", addr: "127.0.0.1:6380"},
+		{err: "REDIRECT [::1]:6380", addr: "[::1]:6380"},
+		{err: "REDIRECT ::1:6380", addr: "[::1]:6380"},
+	} {
+		e := ValkeyError(strmsg('-', c.err))
+		if addr, ok := e.IsRedirect(); !ok || addr != c.addr {
+			t.Fail()
+		}
+	}
+}
+
+func TestIsValkeyRedirect(t *testing.T) {
+	err := errors.New("other")
+	if ret, yes := IsValkeyErr(err); yes {
+		if addr, ok := ret.IsRedirect(); ok {
+			t.Fatalf("TestIsValkeyRedirect fail: expected false, got addr=%s", addr)
+		}
+	}
+
+	valkeyErr := ValkeyError(strmsg('-', "REDIRECT 127.0.0.1:6380"))
+	err = &valkeyErr
+	if ret, yes := IsValkeyErr(err); yes {
+		if addr, ok := ret.IsRedirect(); !ok || addr != "127.0.0.1:6380" {
+			t.Fatalf("TestIsValkeyRedirect fail: expected addr=127.0.0.1:6380, got addr=%s, ok=%t", addr, ok)
+		}
+	} else {
+		t.Fatal("TestIsValkeyRedirect fail: expected ValkeyError")
+	}
+}
+
 func TestIsValkeyBusyGroup(t *testing.T) {
 	err := errors.New("other")
 	if IsValkeyBusyGroup(err) {

pipe_test.go
@@ -546,6 +546,41 @@ func TestNewPipe(t *testing.T) {
 		n1.Close()
 		n2.Close()
 	})
+	t.Run("With EnableRedirect", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3").
+				Reply(slicemsg(
+					'%',
+					[]ValkeyMessage{
+						strmsg('+', "proto"),
+						{typ: ':', intlen: 3},
+					},
+				))
+			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "CAPA", "redirect").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			Standalone: StandaloneOption{
+				EnableRedirect: true,
+			},
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
 }
 
 func TestNewRESP2Pipe(t *testing.T) {
@@ -849,6 +884,52 @@ func TestNewRESP2Pipe(t *testing.T) {
 		if err != io.ErrClosedPipe {
 			t.Fatalf("pipe setup should failed with io.ErrClosedPipe, but got %v", err)
 		}
+	})
+	t.Run("With EnableRedirect RESP2", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			// First batch: RESP3 attempt
+			mock.Expect("HELLO", "3").
+				ReplyError("ERR unknown command `HELLO`")
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK") // ignored because r2=true
+			mock.Expect("CLIENT", "CAPA", "redirect").
+				ReplyString("OK") // ignored because r2=true
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyString("OK") // last 2 are skipped from error checking
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyString("OK")
+			// Second batch: RESP2 fallback
+			mock.Expect("HELLO", "2").
+				Reply(slicemsg('*', []ValkeyMessage{
+					strmsg('+', "proto"),
+					{typ: ':', intlen: 2},
+				}))
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "CAPA", "redirect").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			DisableCache:  true,
+			ClientNoEvict: true,
+			Standalone: StandaloneOption{
+				EnableRedirect: true,
+			},
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
 	})
 	t.Run("With DisableClientSetInfo", func(t *testing.T) {
 		n1, n2 := net.Pipe()

standalone.go
@@ -3,6 +3,7 @@ package valkey
 import (
 	"context"
 	"math/rand/v2"
+	"sync/atomic"
 	"time"
 
 	"github.com/valkey-io/valkey-go/internal/cmds"
@@ -12,19 +13,25 @@ func newStandaloneClient(opt *ClientOption, connFn connFn, retryer retryHandler)
 	if len(opt.InitAddress) == 0 {
 		return nil, ErrNoAddr
 	}
+
 	p := connFn(opt.InitAddress[0], opt)
 	if err := p.Dial(); err != nil {
 		return nil, err
 	}
 	s := &standalone{
-		toReplicas: opt.SendToReplicas,
-		primary:    newSingleClientWithConn(p, cmds.NewBuilder(cmds.NoSlot), !opt.DisableRetry, opt.DisableCache, retryer, false),
-		replicas:   make([]*singleClient, len(opt.Standalone.ReplicaAddress)),
+		toReplicas:     opt.SendToReplicas,
+		replicas:       make([]*singleClient, len(opt.Standalone.ReplicaAddress)),
+		enableRedirect: opt.Standalone.EnableRedirect,
+		connFn:         connFn,
+		opt:            opt,
+		retryer:        retryer,
 	}
+	s.primary.Store(newSingleClientWithConn(p, cmds.NewBuilder(cmds.NoSlot), !opt.DisableRetry, opt.DisableCache, retryer, false))
+
 	for i := range s.replicas {
 		replicaConn := connFn(opt.Standalone.ReplicaAddress[i], opt)
 		if err := replicaConn.Dial(); err != nil {
-			s.primary.Close() // close primary if any replica fails
+			s.primary.Load().Close() // close primary if any replica fails
 			for j := 0; j < i; j++ {
 				s.replicas[j].Close()
 			}
@@ -36,13 +43,18 @@ func newStandaloneClient(opt *ClientOption, connFn connFn, retryer retryHandler)
 }
 
 type standalone struct {
-	toReplicas func(Completed) bool
-	primary    *singleClient
-	replicas   []*singleClient
+	toReplicas     func(Completed) bool
+	primary        atomic.Pointer[singleClient]
+	replicas       []*singleClient
+	enableRedirect bool
+	connFn         connFn
+	opt            *ClientOption
+	retryer        retryHandler
+	redirectCall   call
 }
 
 func (s *standalone) B() Builder {
-	return s.primary.B()
+	return s.primary.Load().B()
 }
 
 func (s *standalone) pick() int {
@@ -52,81 +64,206 @@ func (s *standalone) pick() int {
 	return rand.IntN(len(s.replicas))
 }
 
-func (s *standalone) Do(ctx context.Context, cmd Completed) (resp ValkeyResult) {
-	if s.toReplicas(cmd) {
-		return s.replicas[s.pick()].Do(ctx, cmd)
+func (s *standalone) redirectToPrimary(addr string) error {
+	// Create a new connection to the redirect address
+	redirectOpt := *s.opt
+	redirectOpt.InitAddress = []string{addr}
+	redirectConn := s.connFn(addr, &redirectOpt)
+	if err := redirectConn.Dial(); err != nil {
+		return err
 	}
-	return s.primary.Do(ctx, cmd)
+
+	// Create a new primary client with the redirect connection
+	newPrimary := newSingleClientWithConn(redirectConn, cmds.NewBuilder(cmds.NoSlot), !s.opt.DisableRetry, s.opt.DisableCache, s.retryer, false)
+
+	// Atomically swap the primary and close the old one
+	oldPrimary := s.primary.Swap(newPrimary)
+	oldPrimary.Close()
+
+	return nil
+}
+
+func (s *standalone) Do(ctx context.Context, cmd Completed) (resp ValkeyResult) {
+	attempts := 1
+	if s.enableRedirect {
+		cmd = cmd.Pin()
+	}
+
+retry:
+	if s.toReplicas != nil && s.toReplicas(cmd) {
+		resp = s.replicas[s.pick()].Do(ctx, cmd)
+	} else {
+		resp = s.primary.Load().Do(ctx, cmd)
+	}
+
+	// Handle redirects with retry until context deadline
+	if s.enableRedirect {
+		if ret, yes := IsValkeyErr(resp.Error()); yes {
+			if addr, ok := ret.IsRedirect(); ok {
+				// Command is already pinned at this point
+				err := s.redirectCall.Do(ctx, func() error {
+					return s.redirectToPrimary(addr)
+				})
+				// Use retryHandler to handle multiple redirects with context deadline
+				if err == nil || s.retryer.WaitOrSkipRetry(ctx, attempts, cmd, resp.Error()) {
+					attempts++
+					goto retry
+				}
+			}
+		}
+	}
+
+	return resp
 }
 
 func (s *standalone) DoMulti(ctx context.Context, multi ...Completed) (resp []ValkeyResult) {
+	attempts := 1
+
+	// Pin all commands at the beginning if redirect is enabled
+	if s.enableRedirect {
+		for i := range multi {
+			multi[i] = multi[i].Pin()
+		}
+	}
+
+retry:
 	toReplica := true
 	for _, cmd := range multi {
-		if !s.toReplicas(cmd) {
+		if s.toReplicas == nil || !s.toReplicas(cmd) {
 			toReplica = false
 			break
 		}
 	}
 	if toReplica {
-		return s.replicas[s.pick()].DoMulti(ctx, multi...)
+		resp = s.replicas[s.pick()].DoMulti(ctx, multi...)
+	} else {
+		resp = s.primary.Load().DoMulti(ctx, multi...)
 	}
-	return s.primary.DoMulti(ctx, multi...)
+
+	// Handle redirects with retry until context deadline
+	if s.enableRedirect {
+		for i, result := range resp {
+			if i < len(multi) {
+				if ret, yes := IsValkeyErr(result.Error()); yes {
+					if addr, ok := ret.IsRedirect(); ok {
+						err := s.redirectCall.Do(ctx, func() error {
+							return s.redirectToPrimary(addr)
+						})
+						// Use retryHandler to handle multiple redirects with context deadline
+						if err == nil || s.retryer.WaitOrSkipRetry(ctx, attempts, multi[0], result.Error()) {
+							attempts++
+							goto retry
+						}
+						break // Exit the loop if redirect handling fails
+					}
+				}
+			}
+		}
+	}
+
+	return resp
 }
 
 func (s *standalone) Receive(ctx context.Context, subscribe Completed, fn func(msg PubSubMessage)) error {
-	if s.toReplicas(subscribe) {
+	if s.toReplicas != nil && s.toReplicas(subscribe) {
 		return s.replicas[s.pick()].Receive(ctx, subscribe, fn)
 	}
-	return s.primary.Receive(ctx, subscribe, fn)
+	return s.primary.Load().Receive(ctx, subscribe, fn)
 }
 
 func (s *standalone) Close() {
-	s.primary.Close()
+	s.primary.Load().Close()
 	for _, replica := range s.replicas {
 		replica.Close()
 	}
 }
 
 func (s *standalone) DoCache(ctx context.Context, cmd Cacheable, ttl time.Duration) (resp ValkeyResult) {
-	return s.primary.DoCache(ctx, cmd, ttl)
+	return s.primary.Load().DoCache(ctx, cmd, ttl)
 }
 
 func (s *standalone) DoMultiCache(ctx context.Context, multi ...CacheableTTL) (resp []ValkeyResult) {
-	return s.primary.DoMultiCache(ctx, multi...)
+	if s.enableRedirect {
+		for i := range multi {
+			multi[i].Cmd = multi[i].Cmd.Pin()
+		}
+	}
+	return s.primary.Load().DoMultiCache(ctx, multi...)
 }
 
 func (s *standalone) DoStream(ctx context.Context, cmd Completed) ValkeyResultStream {
-	if s.toReplicas(cmd) {
-		return s.replicas[s.pick()].DoStream(ctx, cmd)
+	if s.enableRedirect {
+		cmd = cmd.Pin()
 	}
-	return s.primary.DoStream(ctx, cmd)
+	var stream ValkeyResultStream
+	if s.toReplicas != nil && s.toReplicas(cmd) {
+		stream = s.replicas[s.pick()].DoStream(ctx, cmd)
+	} else {
+		stream = s.primary.Load().DoStream(ctx, cmd)
+	}
+
+	// Handle redirect for stream
+	if s.enableRedirect && stream.Error() != nil {
+		if ret, yes := IsValkeyErr(stream.Error()); yes {
+			if addr, ok := ret.IsRedirect(); ok {
+				err := s.redirectCall.Do(ctx, func() error {
+					return s.redirectToPrimary(addr)
+				})
+				if err == nil {
+					// Execute the command on the updated primary
+					return s.primary.Load().DoStream(ctx, cmd)
+				}
+			}
+		}
+	}
+
+	return stream
 }
 
 func (s *standalone) DoMultiStream(ctx context.Context, multi ...Completed) MultiValkeyResultStream {
+	var stream MultiValkeyResultStream
 	toReplica := true
 	for _, cmd := range multi {
-		if !s.toReplicas(cmd) {
+		if s.toReplicas == nil || !s.toReplicas(cmd) {
 			toReplica = false
 			break
 		}
 	}
 	if toReplica {
-		return s.replicas[s.pick()].DoMultiStream(ctx, multi...)
+		stream = s.replicas[s.pick()].DoMultiStream(ctx, multi...)
+	} else {
+		stream = s.primary.Load().DoMultiStream(ctx, multi...)
 	}
-	return s.primary.DoMultiStream(ctx, multi...)
+
+	// Handle redirect for stream
+	if s.enableRedirect && stream.Error() != nil {
+		if ret, yes := IsValkeyErr(stream.Error()); yes {
+			if addr, ok := ret.IsRedirect(); ok {
+				err := s.redirectCall.Do(ctx, func() error {
+					return s.redirectToPrimary(addr)
+				})
+				if err == nil {
+					// Execute the command on the updated primary
+					return s.primary.Load().DoMultiStream(ctx, multi...)
+				}
+			}
+		}
+	}
+
+	return stream
 }
 
 func (s *standalone) Dedicated(fn func(DedicatedClient) error) (err error) {
-	return s.primary.Dedicated(fn)
+	return s.primary.Load().Dedicated(fn)
 }
 
 func (s *standalone) Dedicate() (client DedicatedClient, cancel func()) {
-	return s.primary.Dedicate()
+	return s.primary.Load().Dedicate()
 }
 
 func (s *standalone) Nodes() map[string]Client {
 	nodes := make(map[string]Client, len(s.replicas)+1)
-	for addr, client := range s.primary.Nodes() {
+	for addr, client := range s.primary.Load().Nodes() {
 		nodes[addr] = client
 	}
 	for _, replica := range s.replicas {

redis/rueidis

Signed-off-by: Rueian <rueiancsie@gmail.com>

message.go
@@ -88,6 +88,14 @@ func (r *RedisError) IsAsk() (addr string, ok bool) {
 	return
 }
 
+// IsRedirect checks if it is a redis REDIRECT message and returns redirect address.
+func (r *RedisError) IsRedirect() (addr string, ok bool) {
+	if ok = strings.HasPrefix(r.string(), "REDIRECT"); ok {
+		addr = fixIPv6HostPort(strings.Split(r.string(), " ")[1])
+	}
+	return
+}
+
 func fixIPv6HostPort(addr string) string {
 	if strings.IndexByte(addr, '.') < 0 && len(addr) > 0 && addr[0] != '[' { // skip ipv4 and enclosed ipv6
 		if i := strings.LastIndexByte(addr, ':'); i >= 0 {

message_test.go
@@ -109,6 +109,41 @@ func TestRedisErrorIsAsk(t *testing.T) {
 	}
 }
 
+func TestRedisErrorIsRedirect(t *testing.T) {
+	for _, c := range []struct {
+		err  string
+		addr string
+	}{
+		{err: "REDIRECT 127.0.0.1:6380", addr: "127.0.0.1:6380"},
+		{err: "REDIRECT [::1]:6380", addr: "[::1]:6380"},
+		{err: "REDIRECT ::1:6380", addr: "[::1]:6380"},
+	} {
+		e := RedisError(strmsg('-', c.err))
+		if addr, ok := e.IsRedirect(); !ok || addr != c.addr {
+			t.Fail()
+		}
+	}
+}
+
+func TestIsRedisRedirect(t *testing.T) {
+	err := errors.New("other")
+	if ret, yes := IsRedisErr(err); yes {
+		if addr, ok := ret.IsRedirect(); ok {
+			t.Fatalf("TestIsRedisRedirect fail: expected false, got addr=%s", addr)
+		}
+	}
+
+	redisErr := RedisError(strmsg('-', "REDIRECT 127.0.0.1:6380"))
+	err = &redisErr
+	if ret, yes := IsRedisErr(err); yes {
+		if addr, ok := ret.IsRedirect(); !ok || addr != "127.0.0.1:6380" {
+			t.Fatalf("TestIsRedisRedirect fail: expected addr=127.0.0.1:6380, got addr=%s, ok=%t", addr, ok)
+		}
+	} else {
+		t.Fatal("TestIsRedisRedirect fail: expected RedisError")
+	}
+}
+
 func TestIsRedisBusyGroup(t *testing.T) {
 	err := errors.New("other")
 	if IsRedisBusyGroup(err) {

pipe_test.go
@@ -546,6 +546,41 @@ func TestNewPipe(t *testing.T) {
 		n1.Close()
 		n2.Close()
 	})
+	t.Run("With EnableRedirect", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3").
+				Reply(slicemsg(
+					'%',
+					[]RedisMessage{
+						strmsg('+', "proto"),
+						{typ: ':', intlen: 3},
+					},
+				))
+			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "CAPA", "redirect").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			Standalone: StandaloneOption{
+				EnableRedirect: true,
+			},
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
 }
 
 func TestNewRESP2Pipe(t *testing.T) {
@@ -849,6 +884,52 @@ func TestNewRESP2Pipe(t *testing.T) {
 		if err != io.ErrClosedPipe {
 			t.Fatalf("pipe setup should failed with io.ErrClosedPipe, but got %v", err)
 		}
+	})
+	t.Run("With EnableRedirect RESP2", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			// First batch: RESP3 attempt
+			mock.Expect("HELLO", "3").
+				ReplyError("ERR unknown command `HELLO`")
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK") // ignored because r2=true
+			mock.Expect("CLIENT", "CAPA", "redirect").
+				ReplyString("OK") // ignored because r2=true
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyString("OK") // last 2 are skipped from error checking
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyString("OK")
+			// Second batch: RESP2 fallback
+			mock.Expect("HELLO", "2").
+				Reply(slicemsg('*', []RedisMessage{
+					strmsg('+', "proto"),
+					{typ: ':', intlen: 2},
+				}))
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "CAPA", "redirect").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", LibName).
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", LibVer).
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			DisableCache:  true,
+			ClientNoEvict: true,
+			Standalone: StandaloneOption{
+				EnableRedirect: true,
+			},
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
 	})
 	t.Run("With DisableClientSetInfo", func(t *testing.T) {
 		n1, n2 := net.Pipe()

standalone.go
@@ -3,6 +3,7 @@ package rueidis
 import (
 	"context"
 	"math/rand/v2"
+	"sync/atomic"
 	"time"
 
 	"github.com/redis/rueidis/internal/cmds"
@@ -12,19 +13,25 @@ func newStandaloneClient(opt *ClientOption, connFn connFn, retryer retryHandler)
 	if len(opt.InitAddress) == 0 {
 		return nil, ErrNoAddr
 	}
+
 	p := connFn(opt.InitAddress[0], opt)
 	if err := p.Dial(); err != nil {
 		return nil, err
 	}
 	s := &standalone{
-		toReplicas: opt.SendToReplicas,
-		primary:    newSingleClientWithConn(p, cmds.NewBuilder(cmds.NoSlot), !opt.DisableRetry, opt.DisableCache, retryer, false),
-		replicas:   make([]*singleClient, len(opt.Standalone.ReplicaAddress)),
+		toReplicas:     opt.SendToReplicas,
+		replicas:       make([]*singleClient, len(opt.Standalone.ReplicaAddress)),
+		enableRedirect: opt.Standalone.EnableRedirect,
+		connFn:         connFn,
+		opt:            opt,
+		retryer:        retryer,
 	}
+	s.primary.Store(newSingleClientWithConn(p, cmds.NewBuilder(cmds.NoSlot), !opt.DisableRetry, opt.DisableCache, retryer, false))
+
 	for i := range s.replicas {
 		replicaConn := connFn(opt.Standalone.ReplicaAddress[i], opt)
 		if err := replicaConn.Dial(); err != nil {
-			s.primary.Close() // close primary if any replica fails
+			s.primary.Load().Close() // close primary if any replica fails
 			for j := 0; j < i; j++ {
 				s.replicas[j].Close()
 			}
@@ -36,13 +43,18 @@ func newStandaloneClient(opt *ClientOption, connFn connFn, retryer retryHandler)
 }
 
 type standalone struct {
-	toReplicas func(Completed) bool
-	primary    *singleClient
-	replicas   []*singleClient
+	toReplicas     func(Completed) bool
+	primary        atomic.Pointer[singleClient]
+	replicas       []*singleClient
+	enableRedirect bool
+	connFn         connFn
+	opt            *ClientOption
+	retryer        retryHandler
+	redirectCall   call
 }
 
 func (s *standalone) B() Builder {
-	return s.primary.B()
+	return s.primary.Load().B()
 }
 
 func (s *standalone) pick() int {
@@ -52,81 +64,206 @@ func (s *standalone) pick() int {
 	return rand.IntN(len(s.replicas))
 }
 
-func (s *standalone) Do(ctx context.Context, cmd Completed) (resp RedisResult) {
-	if s.toReplicas(cmd) {
-		return s.replicas[s.pick()].Do(ctx, cmd)
+func (s *standalone) redirectToPrimary(addr string) error {
+	// Create a new connection to the redirect address
+	redirectOpt := *s.opt
+	redirectOpt.InitAddress = []string{addr}
+	redirectConn := s.connFn(addr, &redirectOpt)
+	if err := redirectConn.Dial(); err != nil {
+		return err
 	}
-	return s.primary.Do(ctx, cmd)
+
+	// Create a new primary client with the redirect connection
+	newPrimary := newSingleClientWithConn(redirectConn, cmds.NewBuilder(cmds.NoSlot), !s.opt.DisableRetry, s.opt.DisableCache, s.retryer, false)
+
+	// Atomically swap the primary and close the old one
+	oldPrimary := s.primary.Swap(newPrimary)
+	oldPrimary.Close()
+
+	return nil
+}
+
+func (s *standalone) Do(ctx context.Context, cmd Completed) (resp RedisResult) {
+	attempts := 1
+	if s.enableRedirect {
+		cmd = cmd.Pin()
+	}
+
+retry:
+	if s.toReplicas != nil && s.toReplicas(cmd) {
+		resp = s.replicas[s.pick()].Do(ctx, cmd)
+	} else {
+		resp = s.primary.Load().Do(ctx, cmd)
+	}
+
+	// Handle redirects with retry until context deadline
+	if s.enableRedirect {
+		if ret, yes := IsRedisErr(resp.Error()); yes {
+			if addr, ok := ret.IsRedirect(); ok {
+				// Command is already pinned at this point
+				err := s.redirectCall.Do(ctx, func() error {
+					return s.redirectToPrimary(addr)
+				})
+				// Use retryHandler to handle multiple redirects with context deadline
+				if err == nil || s.retryer.WaitOrSkipRetry(ctx, attempts, cmd, resp.Error()) {
+					attempts++
+					goto retry
+				}
+			}
+		}
+	}
+
+	return resp
 }
 
 func (s *standalone) DoMulti(ctx context.Context, multi ...Completed) (resp []RedisResult) {
+	attempts := 1
+
+	// Pin all commands at the beginning if redirect is enabled
+	if s.enableRedirect {
+		for i := range multi {
+			multi[i] = multi[i].Pin()
+		}
+	}
+
+retry:
 	toReplica := true
 	for _, cmd := range multi {
-		if !s.toReplicas(cmd) {
+		if s.toReplicas == nil || !s.toReplicas(cmd) {
 			toReplica = false
 			break
 		}
 	}
 	if toReplica {
-		return s.replicas[s.pick()].DoMulti(ctx, multi...)
+		resp = s.replicas[s.pick()].DoMulti(ctx, multi...)
+	} else {
+		resp = s.primary.Load().DoMulti(ctx, multi...)
 	}
-	return s.primary.DoMulti(ctx, multi...)
+
+	// Handle redirects with retry until context deadline
+	if s.enableRedirect {
+		for i, result := range resp {
+			if i < len(multi) {
+				if ret, yes := IsRedisErr(result.Error()); yes {
+					if addr, ok := ret.IsRedirect(); ok {
+						err := s.redirectCall.Do(ctx, func() error {
+							return s.redirectToPrimary(addr)
+						})
+						// Use retryHandler to handle multiple redirects with context deadline
+						if err == nil || s.retryer.WaitOrSkipRetry(ctx, attempts, multi[0], result.Error()) {
+							attempts++
+							goto retry
+						}
+						break // Exit the loop if redirect handling fails
+					}
+				}
+			}
+		}
+	}
+
+	return resp
 }
 
 func (s *standalone) Receive(ctx context.Context, subscribe Completed, fn func(msg PubSubMessage)) error {
-	if s.toReplicas(subscribe) {
+	if s.toReplicas != nil && s.toReplicas(subscribe) {
 		return s.replicas[s.pick()].Receive(ctx, subscribe, fn)
 	}
-	return s.primary.Receive(ctx, subscribe, fn)
+	return s.primary.Load().Receive(ctx, subscribe, fn)
 }
 
 func (s *standalone) Close() {
-	s.primary.Close()
+	s.primary.Load().Close()
 	for _, replica := range s.replicas {
 		replica.Close()
 	}
 }
 
 func (s *standalone) DoCache(ctx context.Context, cmd Cacheable, ttl time.Duration) (resp RedisResult) {
-	return s.primary.DoCache(ctx, cmd, ttl)
+	return s.primary.Load().DoCache(ctx, cmd, ttl)
 }
 
 func (s *standalone) DoMultiCache(ctx context.Context, multi ...CacheableTTL) (resp []RedisResult) {
-	return s.primary.DoMultiCache(ctx, multi...)
+	if s.enableRedirect {
+		for i := range multi {
+			multi[i].Cmd = multi[i].Cmd.Pin()
+		}
+	}
+	return s.primary.Load().DoMultiCache(ctx, multi...)
 }
 
 func (s *standalone) DoStream(ctx context.Context, cmd Completed) RedisResultStream {
-	if s.toReplicas(cmd) {
-		return s.replicas[s.pick()].DoStream(ctx, cmd)
+	if s.enableRedirect {
+		cmd = cmd.Pin()
 	}
-	return s.primary.DoStream(ctx, cmd)
+	var stream RedisResultStream
+	if s.toReplicas != nil && s.toReplicas(cmd) {
+		stream = s.replicas[s.pick()].DoStream(ctx, cmd)
+	} else {
+		stream = s.primary.Load().DoStream(ctx, cmd)
+	}
+
+	// Handle redirect for stream
+	if s.enableRedirect && stream.Error() != nil {
+		if ret, yes := IsRedisErr(stream.Error()); yes {
+			if addr, ok := ret.IsRedirect(); ok {
+				err := s.redirectCall.Do(ctx, func() error {
+					return s.redirectToPrimary(addr)
+				})
+				if err == nil {
+					// Execute the command on the updated primary
+					return s.primary.Load().DoStream(ctx, cmd)
+				}
+			}
+		}
+	}
+
+	return stream
 }
 
 func (s *standalone) DoMultiStream(ctx context.Context, multi ...Completed) MultiRedisResultStream {
+	var stream MultiRedisResultStream
 	toReplica := true
 	for _, cmd := range multi {
-		if !s.toReplicas(cmd) {
+		if s.toReplicas == nil || !s.toReplicas(cmd) {
 			toReplica = false
 			break
 		}
 	}
 	if toReplica {
-		return s.replicas[s.pick()].DoMultiStream(ctx, multi...)
+		stream = s.replicas[s.pick()].DoMultiStream(ctx, multi...)
+	} else {
+		stream = s.primary.Load().DoMultiStream(ctx, multi...)
 	}
-	return s.primary.DoMultiStream(ctx, multi...)
+
+	// Handle redirect for stream
+	if s.enableRedirect && stream.Error() != nil {
+		if ret, yes := IsRedisErr(stream.Error()); yes {
+			if addr, ok := ret.IsRedirect(); ok {
+				err := s.redirectCall.Do(ctx, func() error {
+					return s.redirectToPrimary(addr)
+				})
+				if err == nil {
+					// Execute the command on the updated primary
+					return s.primary.Load().DoMultiStream(ctx, multi...)
+				}
+			}
+		}
+	}
+
+	return stream
 }
 
 func (s *standalone) Dedicated(fn func(DedicatedClient) error) (err error) {
-	return s.primary.Dedicated(fn)
+	return s.primary.Load().Dedicated(fn)
 }
 
 func (s *standalone) Dedicate() (client DedicatedClient, cancel func()) {
-	return s.primary.Dedicate()
+	return s.primary.Load().Dedicate()
 }
 
 func (s *standalone) Nodes() map[string]Client {
 	nodes := make(map[string]Client, len(s.replicas)+1)
-	for addr, client := range s.primary.Nodes() {
+	for addr, client := range s.primary.Load().Nodes() {
 		nodes[addr] = client
 	}
 	for _, replica := range s.replicas {