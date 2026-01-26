valkey-io/valkey-go

feat: add ability to get AZ from an INFO command (#105)
* feat: add ability to get AZ from an INFO command

Signed-off-by: nickngsr <nick@logicallyus.com>

* docs: update Availability Affinity Routing to include hint about AZFromInfo

Signed-off-by: nickngsr <nick@logicallyus.com>

* fix: use raw string on infoAZ regex

Signed-off-by: nickngsr <nick@logicallyus.com>

* Update valkey.go

Signed-off-by: Rueian <rueiancsie@gmail.com>

---------

Signed-off-by: nickngsr <nick@logicallyus.com>
Signed-off-by: Rueian <rueiancsie@gmail.com>
Co-authored-by: Nick Rees <nick@logicallyus.com>
Co-authored-by: Rueian <rueiancsie@gmail.com>

pipe_test.go
@@ -76,6 +76,15 @@ func (r *valkeyExpect) ReplyBlobString(replies ...string) *valkeyExpect {
 	return r
 }
 
+func (r *valkeyExpect) ReplyVerbatimString(replies ...string) *valkeyExpect {
+	for _, reply := range replies {
+		if r.err == nil {
+			r.Reply(strmsg('=', reply))
+		}
+	}
+	return r
+}
+
 func (r *valkeyExpect) ReplyError(replies ...string) *valkeyExpect {
 	for _, reply := range replies {
 		if r.err == nil {
@@ -110,7 +119,7 @@ func (r *valkeyMock) Close() {
 func write(o io.Writer, m ValkeyMessage) (err error) {
 	_, err = o.Write([]byte{m.typ})
 	switch m.typ {
-	case '$':
+	case '$', '=':
 		_, _ = o.Write(append([]byte(strconv.Itoa(len(m.string()))), '\r', '\n'))
 		_, err = o.Write(append([]byte(m.string()), '\r', '\n'))
 	case '+', '-', '_':
@@ -251,6 +260,81 @@ func TestNewPipe(t *testing.T) {
 		n1.Close()
 		n2.Close()
 	})
+	t.Run("Auth without Username AZ from INFO", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3", "AUTH", "default", "pa", "SETNAME", "cn").
+				Reply(slicemsg(
+					'%',
+					[]ValkeyMessage{
+						strmsg('+', "proto"),
+						{typ: ':', intlen: 3},
+					},
+				))
+			mock.Expect("INFO", "SERVER").
+				ReplyVerbatimString(`txt:# Server
+redis_version:7.2.4
+server_name:valkey
+valkey_version:7.3.0
+redis_git_sha1:0
+redis_git_dirty:0
+redis_build_id:0
+redis_mode:cluster
+os:Amazon MemoryDB
+arch_bits:64
+monotonic_clock:ARM CNTVCT @ 121 ticks/us
+multiplexing_api:epoll
+atomicvar_api:c11-builtin
+gcc_version:0.0.0
+process_id:1
+run_id:63c3785f34bcf381b8652586ad4e57779ff59ede
+tcp_port:6379
+server_time_usec:1768651545574803
+uptime_in_seconds:1451582
+uptime_in_days:16
+hz:10
+configured_hz:10
+lru_clock:7043865
+executable:-
+config_file:-
+availability_zone:us-west-1a
+`)
+			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
+				ReplyString("OK")
+			mock.Expect("SELECT", "1").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-TOUCH", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", "libname").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", "1").
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			SelectDB:            1,
+			Password:            "pa",
+			ClientName:          "cn",
+			ClientNoEvict:       true,
+			ClientSetInfo:       []string{"libname", "1"},
+			ClientNoTouch:       true,
+			EnableReplicaAZInfo: true,
+			AZFromInfo:          true,
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
 	t.Run("AlwaysRESP2", func(t *testing.T) {
 		n1, n2 := net.Pipe()
 		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
@@ -289,6 +373,85 @@ func TestNewPipe(t *testing.T) {
 			ClientNoTouch: true,
 			AlwaysRESP2:   true,
 			DisableCache:  true,
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
+	t.Run("AlwaysRESP2 AZ from INFO", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &valkeyMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("AUTH", "pa").
+				ReplyString("OK")
+			mock.Expect("HELLO", "2").
+				Reply(slicemsg(
+					'*',
+					[]ValkeyMessage{
+						strmsg('+', "proto"),
+						{typ: ':', intlen: 2},
+					},
+				))
+			mock.Expect("INFO", "SERVER").
+				ReplyBlobString(`# Server
+redis_version:7.2.4
+server_name:valkey
+valkey_version:7.3.0
+redis_git_sha1:0
+redis_git_dirty:0
+redis_build_id:0
+redis_mode:cluster
+os:Amazon MemoryDB
+arch_bits:64
+monotonic_clock:ARM CNTVCT @ 121 ticks/us
+multiplexing_api:epoll
+atomicvar_api:c11-builtin
+gcc_version:0.0.0
+process_id:1
+run_id:63c3785f34bcf381b8652586ad4e57779ff59ede
+tcp_port:6379
+server_time_usec:1768651545574803
+uptime_in_seconds:1451582
+uptime_in_days:16
+hz:10
+configured_hz:10
+lru_clock:7043865
+executable:-
+config_file:-
+availability_zone:us-west-1a
+`)
+			mock.Expect("CLIENT", "SETNAME", "cn").
+				ReplyString("OK")
+			mock.Expect("SELECT", "1").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-TOUCH", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", "libname").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", "1").
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			SelectDB:            1,
+			Password:            "pa",
+			ClientName:          "cn",
+			ClientNoEvict:       true,
+			ClientSetInfo:       []string{"libname", "1"},
+			ClientNoTouch:       true,
+			AlwaysRESP2:         true,
+			DisableCache:        true,
+			EnableReplicaAZInfo: true,
+			AZFromInfo:          true,
 		})
 		if err != nil {
 			t.Fatalf("pipe setup failed: %v", err)

redis/rueidis

pipe_test.go
@@ -76,6 +76,15 @@ func (r *redisExpect) ReplyBlobString(replies ...string) *redisExpect {
 	return r
 }
 
+func (r *redisExpect) ReplyVerbatimString(replies ...string) *redisExpect {
+	for _, reply := range replies {
+		if r.err == nil {
+			r.Reply(strmsg('=', reply))
+		}
+	}
+	return r
+}
+
 func (r *redisExpect) ReplyError(replies ...string) *redisExpect {
 	for _, reply := range replies {
 		if r.err == nil {
@@ -110,7 +119,7 @@ func (r *redisMock) Close() {
 func write(o io.Writer, m RedisMessage) (err error) {
 	_, err = o.Write([]byte{m.typ})
 	switch m.typ {
-	case '$':
+	case '$', '=':
 		_, _ = o.Write(append([]byte(strconv.Itoa(len(m.string()))), '\r', '\n'))
 		_, err = o.Write(append([]byte(m.string()), '\r', '\n'))
 	case '+', '-', '_':
@@ -251,6 +260,81 @@ func TestNewPipe(t *testing.T) {
 		n1.Close()
 		n2.Close()
 	})
+	t.Run("Auth without Username AZ from INFO", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("HELLO", "3", "AUTH", "default", "pa", "SETNAME", "cn").
+				Reply(slicemsg(
+					'%',
+					[]RedisMessage{
+						strmsg('+', "proto"),
+						{typ: ':', intlen: 3},
+					},
+				))
+			mock.Expect("INFO", "SERVER").
+				ReplyVerbatimString(`txt:# Server
+redis_version:7.2.4
+server_name:valkey
+valkey_version:7.3.0
+redis_git_sha1:0
+redis_git_dirty:0
+redis_build_id:0
+redis_mode:cluster
+os:Amazon MemoryDB
+arch_bits:64
+monotonic_clock:ARM CNTVCT @ 121 ticks/us
+multiplexing_api:epoll
+atomicvar_api:c11-builtin
+gcc_version:0.0.0
+process_id:1
+run_id:63c3785f34bcf381b8652586ad4e57779ff59ede
+tcp_port:6379
+server_time_usec:1768651545574803
+uptime_in_seconds:1451582
+uptime_in_days:16
+hz:10
+configured_hz:10
+lru_clock:7043865
+executable:-
+config_file:-
+availability_zone:us-west-1a
+`)
+			mock.Expect("CLIENT", "TRACKING", "ON", "OPTIN").
+				ReplyString("OK")
+			mock.Expect("SELECT", "1").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-TOUCH", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", "libname").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", "1").
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			SelectDB:            1,
+			Password:            "pa",
+			ClientName:          "cn",
+			ClientNoEvict:       true,
+			ClientSetInfo:       []string{"libname", "1"},
+			ClientNoTouch:       true,
+			EnableReplicaAZInfo: true,
+			AZFromInfo:          true,
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
 	t.Run("AlwaysRESP2", func(t *testing.T) {
 		n1, n2 := net.Pipe()
 		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
@@ -289,6 +373,85 @@ func TestNewPipe(t *testing.T) {
 			ClientNoTouch: true,
 			AlwaysRESP2:   true,
 			DisableCache:  true,
+		})
+		if err != nil {
+			t.Fatalf("pipe setup failed: %v", err)
+		}
+		go func() { mock.Expect("PING").ReplyString("OK") }()
+		if p.AZ() != "us-west-1a" {
+			t.Fatalf("unexpected az: %v", p.AZ())
+		}
+		p.Close()
+		mock.Close()
+		n1.Close()
+		n2.Close()
+	})
+	t.Run("AlwaysRESP2 AZ from INFO", func(t *testing.T) {
+		n1, n2 := net.Pipe()
+		mock := &redisMock{buf: bufio.NewReader(n2), conn: n2, t: t}
+		go func() {
+			mock.Expect("AUTH", "pa").
+				ReplyString("OK")
+			mock.Expect("HELLO", "2").
+				Reply(slicemsg(
+					'*',
+					[]RedisMessage{
+						strmsg('+', "proto"),
+						{typ: ':', intlen: 2},
+					},
+				))
+			mock.Expect("INFO", "SERVER").
+				ReplyBlobString(`# Server
+redis_version:7.2.4
+server_name:valkey
+valkey_version:7.3.0
+redis_git_sha1:0
+redis_git_dirty:0
+redis_build_id:0
+redis_mode:cluster
+os:Amazon MemoryDB
+arch_bits:64
+monotonic_clock:ARM CNTVCT @ 121 ticks/us
+multiplexing_api:epoll
+atomicvar_api:c11-builtin
+gcc_version:0.0.0
+process_id:1
+run_id:63c3785f34bcf381b8652586ad4e57779ff59ede
+tcp_port:6379
+server_time_usec:1768651545574803
+uptime_in_seconds:1451582
+uptime_in_days:16
+hz:10
+configured_hz:10
+lru_clock:7043865
+executable:-
+config_file:-
+availability_zone:us-west-1a
+`)
+			mock.Expect("CLIENT", "SETNAME", "cn").
+				ReplyString("OK")
+			mock.Expect("SELECT", "1").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-TOUCH", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "NO-EVICT", "ON").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-NAME", "libname").
+				ReplyString("OK")
+			mock.Expect("CLIENT", "SETINFO", "LIB-VER", "1").
+				ReplyString("OK")
+		}()
+		p, err := newPipe(context.Background(), func(ctx context.Context) (net.Conn, error) { return n1, nil }, &ClientOption{
+			SelectDB:            1,
+			Password:            "pa",
+			ClientName:          "cn",
+			ClientNoEvict:       true,
+			ClientSetInfo:       []string{"libname", "1"},
+			ClientNoTouch:       true,
+			AlwaysRESP2:         true,
+			DisableCache:        true,
+			EnableReplicaAZInfo: true,
+			AZFromInfo:          true,
 		})
 		if err != nil {
 			t.Fatalf("pipe setup failed: %v", err)