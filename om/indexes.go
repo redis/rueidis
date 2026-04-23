1434867de0ee85b74374d1378a9049be2528ea0e
Ariel Chinn <ariel@chinn.email>
2026-04-23 15:02:57 +0300

    CR fixes
    Also removed "failed" from wrapped errors as this results in error messages that say "failed" a lot.

--- om/indexes.go
+++ om/indexes.go
@@ -16,20 +16,20 @@ import (
 func createAndAliasIndex(ctx context.Context, idx string, client rueidis.Client, createCmd func(idx string) cmds.FtCreatePrefixPrefix, cmdFn func(schema FtCreateSchema) rueidis.Completed) error {
 	idxRE, err := regexp.Compile("^" + regexp.QuoteMeta(idx) + "_v(\\d+)$")
 	if err != nil {
-		return fmt.Errorf("failed to compile regular expression: %w", err)
+		return fmt.Errorf("compiling regular expression: %w", err)
 	}
 
 	listCmd := client.B().FtList().Build()
 	listResp, err := client.Do(ctx, listCmd).ToArray()
 	if err != nil {
-		return fmt.Errorf("failed to list indexes: %w", err)
+		return fmt.Errorf("listing indexes: %w", err)
 	}
 
 	currVers := make([]int, 0)
 	for _, message := range listResp {
 		n, err := message.ToString()
 		if err != nil {
-			return fmt.Errorf("FT._LIST retured non-string response: %w", err)
+			return fmt.Errorf("FT._LIST returned non-string response: %w", err)
 		}
 		match := idxRE.FindStringSubmatch(n)
 		if len(match) < 2 {
@@ -37,7 +37,7 @@ func createAndAliasIndex(ctx context.Context, idx string, client rueidis.Client,
 		}
 		ver, err := strconv.Atoi(match[1])
 		if err != nil {
-			return fmt.Errorf("failed converting version number for index %q: %w", n, err)
+			return fmt.Errorf("converting version number for index %q: %w", n, err)
 		}
 		currVers = append(currVers, ver)
 	}
@@ -49,17 +49,17 @@ func createAndAliasIndex(ctx context.Context, idx string, client rueidis.Client,
 
 	// Create the new index
 	if err := client.Do(ctx, cmdFn(createCmd(newIndex).Schema())).Error(); err != nil {
-		return err
+		return fmt.Errorf("creating new index: %w", err)
 	}
 
 	if err := client.Do(ctx, client.B().FtAliasupdate().Alias(idx).Index(newIndex).Build()).Error(); err != nil {
-		return fmt.Errorf("failed to update alias: %w", err)
+		return fmt.Errorf("updating alias: %w", err)
 	}
 
 	for _, ver := range currVers {
 		currIdx := fmt.Sprintf("%s_v%d", idx, ver)
 		if err := client.Do(ctx, client.B().FtDropindex().Index(currIdx).Build()).Error(); err != nil {
-			return fmt.Errorf("failed to drop old index %q: %w", currIdx, err)
+			return fmt.Errorf("dropping old index %q: %w", currIdx, err)
 		}
 	}
 
