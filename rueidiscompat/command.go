valkey-io/valkey-go

Add InfoMap compatibility in Valkey-go (#163)
* Add InfoMap compatibility in Valkey-go

Signed-off-by: kumariam-ai <kumariam@google.com>
Co-authored-by: Omkar Mestry omanges@google.com

* refactor(valkeycompat): move INFO parsing to private parseInfoString in valkeycompat

Signed-off-by: kumariam-ai <kumariam@google.com>
Co-authored-by: Arun Satyarth S <asatyarth@google.com>

---------

Signed-off-by: kumariam-ai <kumariam@google.com>
Co-authored-by: Arun Satyarth S <asatyarth@google.com>

valkeycompat/command.go
@@ -922,6 +922,63 @@ func newStringStringMapCmd(res valkey.ValkeyResult) *StringStringMapCmd {
 	return cmd
 }
 
+type StringStringStringMapCmd struct {
+	baseCmd[map[string]map[string]string]
+}
+
+func (cmd *StringStringStringMapCmd) from(res valkey.ValkeyResult) {
+	val, err := res.ToString()
+	cmd.SetErr(err)
+	if err != nil {
+		return
+	}
+	cmd.SetVal(parseInfoString(val))
+	cmd.setIsCacheHit(res.IsCacheHit())
+}
+
+func newStringStringStringMapCmd(res valkey.ValkeyResult) *StringStringStringMapCmd {
+	cmd := &StringStringStringMapCmd{}
+	cmd.from(res)
+	return cmd
+}
+
+func parseInfoString(txt string) map[string]map[string]string {
+	out := make(map[string]map[string]string)
+	section := "default"
+	for _, l := range strings.Split(txt, "\n") {
+		line := strings.TrimSpace(l)
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(strings.ToLower(line), "txt:") {
+			line = strings.TrimSpace(line[len("txt:"):])
+		}
+		if strings.HasPrefix(line, "#") {
+			sec := strings.TrimSpace(strings.TrimLeft(line, "#"))
+			if sec == "" {
+				section = "default"
+			} else {
+				section = sec
+			}
+			continue
+		}
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		k := strings.TrimSpace(parts[0])
+		v := strings.TrimSpace(parts[1])
+		if k == "" {
+			continue
+		}
+		if _, ok := out[section]; !ok {
+			out[section] = make(map[string]string)
+		}
+		out[section][k] = v
+	}
+	return out
+}
+
 // Scan scans the results from the map into a destination struct. The map keys
 // are matched in the Valkey struct fields by the `valkey:"field"` tag.
 func (cmd *StringStringMapCmd) Scan(dest interface{}) error {

redis/rueidis

Signed-off-by: Rueian <rueiancsie@gmail.com>

rueidiscompat/command.go
@@ -922,6 +922,63 @@ func newStringStringMapCmd(res rueidis.RedisResult) *StringStringMapCmd {
 	return cmd
 }
 
+type StringStringStringMapCmd struct {
+	baseCmd[map[string]map[string]string]
+}
+
+func (cmd *StringStringStringMapCmd) from(res rueidis.RedisResult) {
+	val, err := res.ToString()
+	cmd.SetErr(err)
+	if err != nil {
+		return
+	}
+	cmd.SetVal(parseInfoString(val))
+	cmd.setIsCacheHit(res.IsCacheHit())
+}
+
+func newStringStringStringMapCmd(res rueidis.RedisResult) *StringStringStringMapCmd {
+	cmd := &StringStringStringMapCmd{}
+	cmd.from(res)
+	return cmd
+}
+
+func parseInfoString(txt string) map[string]map[string]string {
+	out := make(map[string]map[string]string)
+	section := "default"
+	for _, l := range strings.Split(txt, "\n") {
+		line := strings.TrimSpace(l)
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(strings.ToLower(line), "txt:") {
+			line = strings.TrimSpace(line[len("txt:"):])
+		}
+		if strings.HasPrefix(line, "#") {
+			sec := strings.TrimSpace(strings.TrimLeft(line, "#"))
+			if sec == "" {
+				section = "default"
+			} else {
+				section = sec
+			}
+			continue
+		}
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		k := strings.TrimSpace(parts[0])
+		v := strings.TrimSpace(parts[1])
+		if k == "" {
+			continue
+		}
+		if _, ok := out[section]; !ok {
+			out[section] = make(map[string]string)
+		}
+		out[section][k] = v
+	}
+	return out
+}
+
 // Scan scans the results from the map into a destination struct. The map keys
 // are matched in the Redis struct fields by the `redis:"field"` tag.
 func (cmd *StringStringMapCmd) Scan(dest interface{}) error {