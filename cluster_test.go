valkey-io/valkey-go

perf(cluster): consolidate keyless commands onto one conn on retry (#180)
When rebucketRetries re-picks connections after a topology refresh, a
keyless command (Slot() == InitSlot, e.g. PING) has no slot to route by,
so _pick(InitSlot) returns a connection from an arbitrary position in the
c.conns map iteration. Go re-randomizes map iteration per call, so several
keyless commands in one retry round can scatter across multiple conns,
each spawning its own doretry goroutine and DoMulti round-trip.

Track the last re-picked connection and reuse it for keyless commands, so
they consolidate onto a single conn instead of fanning out. This also
avoids an _pick lock acquisition per keyless command.

prev is only consumed in the per-command branch, never for a MULTI..EXEC
span, so a replica prev can never route a keyless transaction to a
replica. The first command has no prev yet and falls back to _pick. The
DoMultiCache path needs no change since cacheable commands always carry a
real slot.

Signed-off-by: DhruvilK7 <dhruvil.kakadiya@zomato.com>

cluster_test.go
@@ -12335,3 +12335,44 @@ func TestClusterRebucketRetriesCacheAskOnlyBucket(t *testing.T) {
 		t.Fatalf("ASK-only cache bucket must be left untouched, got %+v", retries.m)
 	}
 }
+
+// TestClusterRebucketRetriesKeylessConsolidates verifies that several keyless
+// commands in one bucket re-pick onto a single conn instead of fanning out
+// across the arbitrary conns _pick(InitSlot) would otherwise return.
+func TestClusterRebucketRetriesKeylessConsolidates(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	client := newRebucketTestClient(t)
+	defer client.Close()
+
+	// Three live conns to choose from; oldConn is not among them, so the
+	// keyless commands must move. Without consolidation each could land on a
+	// different one of the three.
+	cA := &mockConn{DoFn: func(cmd Completed) ValkeyResult { return ValkeyResult{} }}
+	cB := &mockConn{DoFn: func(cmd Completed) ValkeyResult { return ValkeyResult{} }}
+	cC := &mockConn{DoFn: func(cmd Completed) ValkeyResult { return ValkeyResult{} }}
+	client.mu.Lock()
+	client.conns = map[string]connrole{"a": {conn: cA}, "b": {conn: cB}, "c": {conn: cC}}
+	client.mu.Unlock()
+
+	oldConn := &mockConn{DoFn: func(cmd Completed) ValkeyResult { return ValkeyResult{} }}
+	retries := connretryp.Get(1, 1)
+	defer connretryp.Put(retries)
+	nr := retryp.Get(0, 3)
+	nr.cIndexes = append(nr.cIndexes, 0, 1, 2)
+	nr.commands = append(nr.commands, client.B().Ping().Build(), client.B().Ping().Build(), client.B().Ping().Build())
+	retries.m[oldConn] = nr
+
+	client.rebucketRetries(retries)
+
+	if _, still := retries.m[oldConn]; still {
+		t.Fatalf("oldConn bucket must be drained, got %+v", retries.m[oldConn])
+	}
+	if len(retries.m) != 1 {
+		t.Fatalf("keyless commands must consolidate onto one conn, got %d buckets", len(retries.m))
+	}
+	for _, bucket := range retries.m {
+		if len(bucket.commands) != 3 {
+			t.Fatalf("expected all 3 keyless commands on one conn, got %d", len(bucket.commands))
+		}
+	}
+}

redis/rueidis

Signed-off-by: Rueian <rueiancsie@gmail.com>

cluster_test.go
@@ -12335,3 +12335,44 @@ func TestClusterRebucketRetriesCacheAskOnlyBucket(t *testing.T) {
 		t.Fatalf("ASK-only cache bucket must be left untouched, got %+v", retries.m)
 	}
 }
+
+// TestClusterRebucketRetriesKeylessConsolidates verifies that several keyless
+// commands in one bucket re-pick onto a single conn instead of fanning out
+// across the arbitrary conns _pick(InitSlot) would otherwise return.
+func TestClusterRebucketRetriesKeylessConsolidates(t *testing.T) {
+	defer ShouldNotLeak(SetupLeakDetection())
+	client := newRebucketTestClient(t)
+	defer client.Close()
+
+	// Three live conns to choose from; oldConn is not among them, so the
+	// keyless commands must move. Without consolidation each could land on a
+	// different one of the three.
+	cA := &mockConn{DoFn: func(cmd Completed) RedisResult { return RedisResult{} }}
+	cB := &mockConn{DoFn: func(cmd Completed) RedisResult { return RedisResult{} }}
+	cC := &mockConn{DoFn: func(cmd Completed) RedisResult { return RedisResult{} }}
+	client.mu.Lock()
+	client.conns = map[string]connrole{"a": {conn: cA}, "b": {conn: cB}, "c": {conn: cC}}
+	client.mu.Unlock()
+
+	oldConn := &mockConn{DoFn: func(cmd Completed) RedisResult { return RedisResult{} }}
+	retries := connretryp.Get(1, 1)
+	defer connretryp.Put(retries)
+	nr := retryp.Get(0, 3)
+	nr.cIndexes = append(nr.cIndexes, 0, 1, 2)
+	nr.commands = append(nr.commands, client.B().Ping().Build(), client.B().Ping().Build(), client.B().Ping().Build())
+	retries.m[oldConn] = nr
+
+	client.rebucketRetries(retries)
+
+	if _, still := retries.m[oldConn]; still {
+		t.Fatalf("oldConn bucket must be drained, got %+v", retries.m[oldConn])
+	}
+	if len(retries.m) != 1 {
+		t.Fatalf("keyless commands must consolidate onto one conn, got %d buckets", len(retries.m))
+	}
+	for _, bucket := range retries.m {
+		if len(bucket.commands) != 3 {
+			t.Fatalf("expected all 3 keyless commands on one conn, got %d", len(bucket.commands))
+		}
+	}
+}